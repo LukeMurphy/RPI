0559f5d8e04b0612bf025f56fe33f9ead6ee80e9
Luke M <web@lukelab.com>
2016-01-31 22:48:22 +0000

    Fixing ...

--- cntrlscripts/run.sh
+++ cntrlscripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ "$1" == "image" ] then 
+if [ "$1" == "image" ]; then 
 	sudo python /home/pi/RPI/image-scroller.py "$@" ; 
-elif [ "$1" == "wb" ] ; then 
+elif [ "$1" == "wb" ]; then 
 	sudo python /home/pi/RPI/whiteboardscroller.py ; 
 else 
 	sudo python /home/pi/RPI/sequence.py "$@" ; 

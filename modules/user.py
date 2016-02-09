88bf8d10abeba250936c4590f90d7b037f0ef6dc
Luke Murphy <web@lukelab.com>
2016-02-09 00:20:15 +0000

    more fixes for better rendering and positioning for multiple users

--- modules/user.py
+++ modules/user.py
@@ -32,19 +32,19 @@ def drawUser(n = 0) :
         bw = 32 * scale
         bh = 32 * scale
 
-        hx1 = centerx + bw/2 - hw/2 + offsetx * scale
+        hx1 = userCenterx + bw/2 - hw/2 + offsetx * scale
         hx2 = hx1 + hw
-        hy1 = centery + offsety * scale
-        hy2 = centery + hh + offsety * scale
+        hy1 = userCentery + offsety * scale
+        hy2 = userCentery + hh + offsety * scale
 
-        mx1 = centerx - mw/2 + bw/2 + offsetx * scale
+        mx1 = userCenterx - mw/2 + bw/2 + offsetx * scale
         mx2 = mx1 + mw
-        my1 = centery + hw * 2/3 + offsety * scale
+        my1 = userCentery + hw * 2/3 + offsety * scale
         my2 = my1
 
-        bx1 = centerx + offsetx * scale
+        bx1 = userCenterx + offsetx * scale
         bx2 = bx1 + bw
-        by1 = centery + hh -3 + offsety * scale
+        by1 = userCentery + hh -3 + offsety * scale
         by2 = 3 + by1 + bh
 
         r=g=b=int(124 * config.brightness)
@@ -52,19 +52,12 @@ def drawUser(n = 0) :
         
         matrix = config.matrix
         draw = config.draw
-        # id = config.id
-        # x1, y1, x2, y2 of bounding box
 
         #### BODY
-        
-        #draw.arc((10,25,200,200), 0, 135)
-        #draw.ellipse((bx1,by1,bx2,by2),fill=(r,g,b,1), outline=0)
-        #print(bx1,by1)
-    
         config.draw.ellipse((bx1,by1,bx2,by2),fill=(r,g,b,1), outline=1)
         draw.arc((int(bx1-1),int(by1-1),int(bx2+1),int(by2+1)),180,360,fill=(0,0,0,1))
         #### HEAD
-        config.draw.ellipse((hx1,hy1,hx2,hy2),fill=(r,g,b,1), outline=1)
+        draw.ellipse((hx1,hy1,hx2,hy2),fill=(r,g,b,1), outline=1)
         draw.arc((int(hx1-1),int(hy1-1),int(hx2+1),int(hy2+1)),180,360,fill=(0,0,0,1))
         #### MOUTH
        
@@ -80,22 +73,14 @@ def drawUser(n = 0) :
                 draw.ellipse((hx1,hy1,hx2,hy2),fill=(r,g,b,1), outline=1)
                 draw.arc((int(hx1-1),int(hy1-1),int(hx2+1),int(hy2+1)),180,360,fill=(0,0,0,1))
                 draw.ellipse((mx1,my1- mh/2,mx2,my2 + mh/2), fill=(int(180 * config.brightness),int(80 * config.brightness),int(80 * config.brightness),2), outline=1)
-
-                #matrix.SetImage(config.id, config.screenWidth/2 - centerx,centery)
-                #config.render(config.image,  0, 0, config.screenWidth, config.screenHeight)
                 
         else :
                 draw.line((mx1, my1, mx1+mw, my2), fill=1)
-                #matrix.SetImage(id,config.screenWidth/2  - centerx,centery)
-                #config.render(config.image, 0, 0, config.screenWidth, config.screenHeight)
-
-        config.render(config.image, userCenterx, userCentery, 128, 128)   
-        #config.render(config.image, userCenterx, userCentery, 31, 32, False)      
 
 
 def userAnimator(arg, numUsers=-1, fixed = False) :
         global config, userList, userCenterx, userCentery , scale 
-        config.image = config.Image.new("RGBA", (int(32 * scale), int(32 * scale)))
+        config.image = config.Image.new("RGBA", (config.screenWidth, config.screenHeight))
         config.draw  = config.ImageDraw.Draw(config.image)
         config.draw.rectangle((0,0,int(32 * scale), int(32 * scale)), fill= (0,0,0))
         #config.id = image.im.id
@@ -104,18 +89,19 @@ def userAnimator(arg, numUsers=-1, fixed = False) :
         userList = [[0,0]]*numUsers
 
         xPosInit = 0
-        xMid = config.screenWidth/config.cols
+        xMid = int(32 * scale) #config.screenWidth/config.cols
         for n in range(0,numUsers) :
                 if(fixed == False) : 
                         xPos = int(random.uniform(xPosInit+2,(config.screenWidth-32) * 2/3))
                 else :
-                        xPos = n * xMid
-                userList[n] = [userCenterx + xPos + xPosInit, userCentery]
-                xPosInit =  xPos + 32
+                        xPos = userCenterx + n * (xMid + userCenterx)
+                userList[n] = [xPos + xPosInit, userCentery]
+                #xPosInit =  xPos + 32
 
         count = 0
         while (count < arg) :
                 for n in range(0,numUsers) : drawUser(n)
+                config.render(config.image, 0, 0,128,128)
                 count+=1
                 time.sleep(.25)
         #if(random.random() > .5) :  userAnimator(arg)

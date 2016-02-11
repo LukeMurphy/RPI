45500d7ef2fb4cbd023974a0266d161581fac2e4
Luke Murphy <web@lukelab.com>
2016-02-11 06:54:44 +0000

    adjusting

--- soliloquy.py
+++ soliloquy.py
@@ -12,7 +12,8 @@ class soliloquy() :
         self.name = arg
         self.I = 0
         self.lastI = 1
-        self.msg = ["Oh Infinite beatitude of existence me ",
+        self.msg = [
+        "Oh Infinite beatitude of existence me ",
         "I am; and there is nothing else beside I that is me ",
         "I fill all Space, and what I fill, I am ", 
         "What I think, that I utter; and what I utter, that I hear ",
@@ -21,7 +22,8 @@ class soliloquy() :
         "Ah, the happiness, ah, the happiness of Being "
         "Ah, the joy, ah, the joy of Thought ",
         "Ah that is me all me by me "
-        "What can I not achieve by thinking! My own Thought coming to myself ",
+        "What can I not achieve by thinking!",
+        " My own Thought coming to myself ",
         "Ah, the joy, the joy of Being ", 
         "Me I mine is "
         "Me wonderful me that I am "
@@ -38,27 +40,38 @@ class soliloquy() :
         self.soliloquy()
         '''
 
-        if(random.random() > .2) :
-            rnd = int(random.uniform(1,4))
-            
+        if(random.random() > .93) :
+            rnd = int(random.uniform(1,6))    
             if(rnd == 1) :
                 print('Me. ', end="")
             elif(rnd == 2) :
                 print('Me. ')
             elif(rnd == 3) :
-                print('[I] ', end="")
-        if(random.random() > .8) : print("O Me", end=" ")
-      
-        msgTxt = ""
+                print('I <==> me <==> I', end="")
+            elif(rnd == 4) :
+                print('I && me ', end="")
+            elif(rnd == 5) :
+                print('me <<< me ', end="")
 
-        if(random.random() > .7) :
+        if(random.random() > .98) : print("O Me", end=" ")
+        if(random.random() > .998) : 
+            print("")
+            print("|-------|")
+            print("|   I   |")
+            print("|-------|")
+
+        msgTxt = ""
+        if(random.random() > .99) :
+            print(time.clock())
+        if(random.random() > .97) :
                 self.I = int(random.random() * self.length)
 
                 while (self.I == self.lastI):
                     self.I = int(random.random() * self.length)
                 msgTxt  =  self.msg[self.I]  
         print(msgTxt, end=" ")
-        if(random.random() > .5) : print(" ")
+        if(random.random() > .5) : print(" ", end="")
+        if(random.random() > .85) : print(" ")
         self.lastI = self.I
         sys.stdout.flush()
         
@@ -84,5 +97,8 @@ t.start()
 
 while True :
     s1.soliloquy()
-    delay = random.random() * 1
+
+
+    delay = random.random() * .3
+    #delay = .1
     time.sleep(delay)

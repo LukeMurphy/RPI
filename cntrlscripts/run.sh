de282e584eb22967812cf7cd15325a5df0040219
Luke A <luke@lukelab.com>
2016-02-23 23:07:19 +0000

    adding setsid to script execution to prevent close out

--- cntrlscripts/run.sh
+++ cntrlscripts/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$1" == "image" ]; then 
-	sudo python /home/pi/RPI/image-scroller.py "$@" ; 
+	setsid sudo python /home/pi/RPI/image-scroller.py "$@" ; 
 elif [ "$1" == "wb" ]; then 
-	sudo python /home/pi/RPI/whiteboardscroller.py ; 
+	setsid sudo python /home/pi/RPI/whiteboardscroller.py ; 
 else 
-	sudo python /home/pi/RPI/sequence.py "$@" ; 
+	setsid sudo python /home/pi/RPI/sequence.py "$@" ; 
 fi

daa151e9a65ab4ec4bde36b5abe185eb6853dfbe
Luke Murphy <web@lukelab.com>
2016-03-12 18:28:08 +0000

    adding kill call to just the sequence script to leave button script intact

--- cntrlscripts/off_pause.sh
+++ cntrlscripts/off_pause.sh
@@ -1,2 +1,2 @@
 #
-ps -ef | pgrep python | xargs sudo kill;
+ps aux | grep sequence | awk '{print $2}' | xargs sudo kill -9;
--- cntrlscripts/off_restarts.sh
+++ cntrlscripts/off_restarts.sh
@@ -1,4 +1,4 @@
 #
-ps -eaf | pgrep python | xargs sudo kill
+ps aux | grep sequence | awk '{print $2}' | xargs sudo kill -9;
 sleep 1
 sudo python /home/pi/RPI/sequence.py seq "$@"

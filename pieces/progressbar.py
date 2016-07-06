d19d6b7f97cf6810fb99bf711fea09642d43828c
Luke Murphy <web@lukelab.com>
2016-07-06 01:58:26 +0000

    ==> more refinements on progress bar

--- pieces/progressbar.py
+++ pieces/progressbar.py
@@ -20,15 +20,20 @@ class ProgressBar :
 	rate = 2
 	pauses = 300
 	pauseCount = 0
+	complete = False
+	percentage = 0
+	boxMaxAlt = 0 
 
 	def __init__(self, config):
 		print ("init PB")
 		
 		self.boxMax = config.screenWidth - 2
+		self.boxMaxAlt = self.boxMax + int(random.uniform(1,3) * config.screenWidth)
 		self.boxHeight = config.screenHeight - 3
 		self.holderColor = (5,5,5)
 		self.xPos = 1
 		self.yPos = 1
+		self.lastPause = False
 
 		config.fontSize = int(workConfig.get("progressbar", 'fontSize'))
 		config.vOffset = int(workConfig.get("progressbar", 'vOffset'))
@@ -36,13 +41,20 @@ class ProgressBar :
 		config.steps = int(workConfig.get("progressbar", 'steps'))
 		config.shadowSize = int(workConfig.get("progressbar", 'shadowSize'))
 		config.sansSerif = (workConfig.getboolean("progressbar", 'sansSerif'))
+		self.pauseAt99 = False
+		self.pauseAt100 = False
+		self.goBack = False
+		self.goPast = False
+		self.messageOverride = True
+		self.messageOverrideActive = False
+
 		if(config.sansSerif) : 
 			self.font = ImageFont.truetype(config.path  + '/assets/fonts/freefont/FreeSansBold.ttf', config.fontSize)
 		else :
 			self.font = ImageFont.truetype(config.path  + '/assets/fonts/freefont/FreeSerifBold.ttf', config.fontSize)
 		self.config = config
 
-		self.messageString = "100%"
+		self.messageString = "PLEASE WAIT. UPDATING..."
 		tempImage = Image.new("RGBA", (1200,196))
 		draw  = ImageDraw.Draw(tempImage)
 		self.pixLen = draw.textsize(self.messageString, font = self.font)
@@ -53,22 +65,39 @@ class ProgressBar :
 		self.clr = (255,255,255)
 
 	def reDraw(self) :
-
-		self.boxWidth += self.rate
-		percentage  =  (float(self.boxWidth)/float(self.boxMax)*100)
-		pauseProbability = math.exp(percentage/10) / 40000
-
-		displayPercentage  = int(math.ceil(percentage))
-		self.messageString = str(displayPercentage) + "%"
-
-		
+		pauseProbability = math.exp(self.percentage/10) / 40000
 		if(random.random() < pauseProbability and self.pauseCount < self.pauses) : 
 			self.rate = 0
 			self.pauseCount += 1
 		
+		if(random.random() > .98) : 
+			self.rate = random.random() * 2
 
-		if(random.random() > .95) : self.rate = random.random()
-		
+		if(self.goBack and random.random() > .98) :
+			self.rate *= -1
+
+		self.boxWidth += self.rate
+		self.percentage  =  (float(self.boxWidth)/float(self.boxMax)*100)
+
+		if(self.percentage >= 100 and self.goPast != True) : 
+			self.boxWidth = self.boxMax - 1
+			self.complete = True
+		elif(self.percentage >= 100 and self.goPast and self.boxWidth >= self.boxMaxAlt) :
+			self.complete = True
+
+		self.displayPercentage = int(math.floor(self.percentage))
+		if(self.messageOverrideActive != True) : self.messageString = str(self.displayPercentage) + "%"
+
+		if(random.random() > .9 and self.messageOverride) :
+			self.rate = 0
+			self.messageString = "PLEASE WAIT. UPDATING..."
+			self.messageOverrideActive = True
+
+		if(random.random() > .999 ) : 
+			self.messageOverride = False
+			self.messageOverrideActive = False
+			
+
 		indent  =  0
 		# draw box container
 		config.draw.rectangle((self.xPos-1, self.yPos-1, self.boxMax+1, self.boxHeight+self.yPos+1), 
@@ -80,15 +109,18 @@ class ProgressBar :
 		#self.txtdraw.rectangle((0,0,self.scrollImage.width, self.scrollImage.height), fill=(0,0,0))
 		self.scrollImage = Image.new("RGBA", (self.pixLen[0] + 2 , self.fontHeight))
 		self.txtdraw  = ImageDraw.Draw(self.scrollImage)
-		for i in range(1, self.config.shadowSize + 1) :
+		for i in range(1, self.config.shadowSize ) :
 			self.txtdraw.text((indent + -i,-i),self.messageString,(0,0,0),font=self.font)
 			self.txtdraw.text((indent + i,i),self.messageString,(0,0,0),font=self.font)
-			self.txtdraw.text((0,0),self.messageString, self.clr ,font=self.font)
-		config.image.paste(self.scrollImage, (self.boxMax - self.pixLen[0] - 4,24), self.scrollImage)
+		self.txtdraw.text((0,0),self.messageString, self.clr ,font=self.font)
 
-		if(percentage >= 100) :
-			time.sleep(int(random.random() * 10))
-			self.done()
+		numXPos = self.boxMax - self.pixLen[0] - 4
+
+		numXPos = 32
+		numYPos = 24
+
+		config.image.paste(self.scrollImage, (numXPos, numYPos), self.scrollImage)
+
 
 	def drawBar(self) :
 		config = self.config
@@ -98,6 +130,24 @@ class ProgressBar :
 		self.pauseCount = 0
 		self.boxWidth = 1
 		self.rate = 2
+		self.lastPause = False
+		self.complete = False
+
+		self.pauseAt99 = True if (random.random() > .8) else False
+		self.pauseAt100 = True if (random.random() > .9) else False
+		self.goBack = True if (random.random() > .9) else False
+		self.goPast = True if (random.random() > .9) else False
+		self.messageOverride = True if (random.random() > .98) else False
+		self.messageOverrideActive = False
+
+		if(self.goPast) : 
+			self.pauseAt100 = False
+			self.pauseAt99 = True
+			self.goBack = False
+
+		print(self.pauseAt100,self.pauseAt99,self.goBack,self.goPast)
+
+
 
 def drawElement() :
 	global config
@@ -128,9 +178,20 @@ def runWork():
 		time.sleep(redrawSpeed)
 
 def iterate() :
-	global config
+	global config, pBar
+	
 	redraw()
 	config.render(config.image, 0, 0, config.screenWidth, config.screenHeight)
+
+	if(pBar.complete) : 
+		if(pBar.pauseAt100) : time.sleep(int(random.uniform(3,10)))
+		time.sleep(2)
+		pBar.done()
+
+	if(pBar.percentage >= 99 and pBar.percentage < 100 and pBar.pauseAt99 and pBar.pauseCount < pBar.pauses) :
+		time.sleep(int(random.uniform(3,10)))
+		pBar.pauseCount = pBar.pauses
+
 	callBack()
 	count = 0
 	# Done

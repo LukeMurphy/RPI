b7839bf2f1ae4a3ec3912d017b324b7fb1452682
Luke Murphy <luke@lukelab.com>
2016-11-08 16:48:57 -0500

    ==> adjustments for linux box to hub

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/bin/python /home/lukr/Documents/RPI/player.py pipi /home/lukr/Documents/RPI/ /home/lukr/Documents/RPI/configs/studio/fludd-1.1.cfg&
+/usr/bin/python /home/lukr/Documents/RPI/player.py pipi /home/lukr/Documents/RPI/ /home/lukr/Documents/RPI/configs/fludd.cfg&
 #/usr/bin/python /home/lukr/Documents/RPI/player.py pipi /home/lukr/Documents/RPI/ /home/lukr/Documents/RPI/configs/studio/repeater-square-1.1.cfg&

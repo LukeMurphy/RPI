5bb3e964eae21b0b769956c8e1c96dcaf345db8c
Luke Murphy <web@lukelab.com>
2016-11-13 12:31:09 -0500

    ==> adding specific startup script for larger square ==> small pixel adjust for repeater

---
+++ cntrlscripts/startup_feelings.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/bin/python /home/daemon3/Documents/RPI/player.py dem3 /home/daemon3/Documents/RPI/ /home/daemon3/Documents/RPI/configs/lrgsq/feeling.cfg&
+#/usr/bin/python /home/lukr/Documents/RPI/player.py pipi /home/lukr/Documents/RPI/ /home/lukr/Documents/RPI/configs/studio/repeater-square-1.1.cfg&

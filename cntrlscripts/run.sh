187916ac51c36853bdc7098fa183ad87f698cab7
Luke Murphy <web@lukelab.com>
2017-01-26 00:24:31 +0000

    ==> new cfg

--- cntrlscripts/run.sh
+++ cntrlscripts/run.sh
@@ -2,5 +2,5 @@
 if [ "$1" != "" ]; then 
 	setsid sudo python /home/pi/RPI/sequenceplayer.py "$@" & 
 else 
-	setsid sudo python /home/pi/RPI/player.py RPI9 /home/pi/RPI/ /home/pi/RPI/configs/progressbar-1.1.cfg&
+	setsid sudo python /home/pi/RPI/player.py RPI9 /home/pi/RPI/ /home/pi/RPI/configs/pb2b.cfg&
 fi

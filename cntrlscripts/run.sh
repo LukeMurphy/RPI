d53c6abc56ca645b4b33c32b2ee2bb77b976cc1e
Luke Murphy <web@lukelab.com>
2017-01-26 00:23:01 +0000

    ==> adding the local config

--- cntrlscripts/run.sh
+++ cntrlscripts/run.sh
@@ -2,5 +2,5 @@
 if [ "$1" != "" ]; then 
 	setsid sudo python /home/pi/RPI/sequenceplayer.py "$@" & 
 else 
-	setsid sudo python /home/pi/RPI/player.py RPI9 /home/pi/RPI/ /home/pi/RPI/configs/studio/h-hat-base-1.1.cfg&
+	setsid sudo python /home/pi/RPI/player.py RPI9 /home/pi/RPI/ /home/pi/RPI/configs/progressbar-1.1.cfg&
 fi

636c5d4331de3edc95361a4635b1560a27b2d39f
Luke Murphy <luke@lukelab.com>
2017-02-03 12:47:06 -0500

    ==> configs for lukr bric box

---
+++ cntrlscripts/lukrstartup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python /home/lukr/Documents/RPI/player.py RPI9 /home/lukr/Documents/RPI/ /home/lukr/Documents/RPI/configs/pb2.cfg&
+python /home/lukr/Documents/RPI/player.py RPI9 /home/lukr/Documents/RPI/ /home/lukr/Documents/RPI/configs/fludd.cfg&
+

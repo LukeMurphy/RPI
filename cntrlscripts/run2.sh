db5a4efbbdaf048e35bfdcec70405f32099b9f47
Luke Murphy <web@lukelab.com>
2017-02-06 15:30:25 -0500

    ==> changed location of local repo

--- cntrlscripts/run2.sh
+++ cntrlscripts/run2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo python /Users/lamshell/Documents/Dev/LED-MATRIX-RPI/RPI/player.py studio-mac ./ configs/"$@".cfg & 
+sudo python /Users/lamshell/Documents/Dev/RPI/player.py studio-mac ./ configs/"$@".cfg & 

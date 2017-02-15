13a042c48137112e01a390a124686d4045bbbbc5
Luke Murphy <lukemurphy@lmurphymbookair.ascap.com>
2017-02-15 18:31:42 -0500

    ==> mods

--- pieces/marquee.py
+++ pieces/marquee.py
@@ -6,8 +6,15 @@ from PIL import ImageFont, Image, ImageDraw, ImageOps, ImageEnhance
 from modules import colorutils
 
 
-def guess():
-	pass
+def makeMarquee(p,w,h):
+	perimeter = []
+	for i in range (p[0], p[0] + w) : perimeter.append([i, p[1]])
+	for i in range (p[1], p[1] + h) : perimeter.append([p[0] + w, i])
+	for i in range (p[0] + w - 1, p[0], -1) : perimeter.append([i, p[1] + h])
+	for i in range (p[1] + h - 1, p[1], -1) : perimeter.append([p[0], i])
+	return (perimeter)
+
+
 
 def init() :
 	global config
@@ -22,25 +29,32 @@ def init() :
 	config.draw.rectangle((0,0,config.screenWidth,config.screenHeight), fill=(0,0,0,255))
 
 
-	w = config.screenWidth
-	h = config.screenHeight
+	config.marquees = []
+	offset = 0
+	marqueeWidth = 6
+	w = config.screenWidth - marqueeWidth
+	h = config.screenHeight - marqueeWidth
 
-	config.perimeter = []
-
-
-	for i in range (0, w) : config.perimeter.append([i, 0])
-	for i in range (0, h) : config.perimeter.append([w - 1 - config.marqueeWidth, i])
-	for i in range (w - 1, 0, -1) : config.perimeter.append([i, h - 1 - config.marqueeWidth])
-	for i in range (h - 1, config.marqueeWidth, -1) : config.perimeter.append([0, i])
-
+	pattern = [1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0]
+	clrs = [(255,0,0),(0,255,0)]
+	config.marquees.append([0, pattern, makeMarquee((0,0),w,h), marqueeWidth , clrs])
+	pattern = [0,0,0,0,0,0,0,0,1,1,1,1,1,1]
+	clrs = [(255,0,0),(0,255,0)]
+	config.marquees.append([0, pattern, makeMarquee((marqueeWidth,marqueeWidth),w-marqueeWidth * 2,h-marqueeWidth * 2), marqueeWidth, clrs ])
+	
+	
+	pattern = [1,1,1,0,0,0,0,0]
+	clrs = [(0,0,255),(255,255,0)]
+	config.marquees.append([0, pattern, makeMarquee((marqueeWidth * 2,marqueeWidth * 2),w-marqueeWidth * 4,h-marqueeWidth * 4), marqueeWidth, clrs ])
+	pattern = [1,1,1,1,0,0,0]
+	clrs = [(255,0,255),(0,255,0)]
+	config.marquees.append([0, pattern, makeMarquee((marqueeWidth * 3,marqueeWidth * 3),w-marqueeWidth * 6,h-marqueeWidth * 6), marqueeWidth, clrs ])
 	
 
-	config.pattern = [1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0]
-	config.offset = 0
-
 	gap = 0
+
 	for i in range(0, len(config.word)) :
-		startX = config.textPosX + 10*i
+		startX = config.textPosX + 10 * i
 		endX = startX + 8
 		#config.draw.line((startX, config.textPosY + 20 , endX, config.textPosY + 20), fill=config.clr)
 
@@ -71,42 +85,36 @@ def drawElement() :
 
 def redraw():
 	global config
-	#drawText(10, 10, "TEST", False)
 	config.draw.rectangle((0,0,config.screenWidth,config.screenHeight), fill=(0,0,0,255))
-	
 
-	#config.offset = 1
+	for m in config.marquees :
 
-	l = len(config.pattern)
+		offset = m[0]
+		pattern = m[1]
+		perimeter = m[2]
+		marqueeWidth = m[3]
+		l = len(pattern)
 
-	patternA = config.pattern[0 : (l - config.offset)]
-	patternB = config.pattern[(l - config.offset): l ]
-	pattern = patternB + patternA
+		patternA = pattern[0 : (l - offset)]
+		patternB = pattern[(l - offset): l ]
+		pattern = patternB + patternA
+		clrA = m[4][0]
+		clrB = m[4][1]
 
-	#print(patternA)
-	#print(patternB)
-	#drawText(10, 10, str(config.offset) + str(pattern), False)
+		count = 0
 
-	#exit()
+		for p in reversed(perimeter):
+			if(pattern[count] == 1) :
+				config.draw.rectangle((p[0],p[1],p[0] + marqueeWidth, p[1] + marqueeWidth), fill=clrA)
+			else:
+				config.draw.rectangle((p[0],p[1],p[0] + marqueeWidth, p[1] + marqueeWidth), fill=clrB)
+			count += 1
+			if(count >= len(pattern)) :
+				count = 0
 
-	count = 0
-	for p in config.perimeter :
-		if(pattern[count] == 1) :
-			config.draw.rectangle((p[0],p[1],p[0] + config.marqueeWidth, p[1] + config.marqueeWidth), fill=(255,0,0))
-		else:
-			config.draw.rectangle((p[0],p[1],p[0] + config.marqueeWidth, p[1] + config.marqueeWidth), fill=(0,255,0))
-		count += 1
-		if(count >= len(config.pattern)) :
-			count = 0
+		m[0] += 1
+		if(m[0] >= len(pattern)) : m[0] =  0 
 
-	
-	config.offset += 1
-	if(config.offset >= len(config.pattern)) : 
-		config.offset =  0 #int(len(config.pattern)/2) 
-		#config.pattern = config.pattern[:-1]
-
-	#drawText(10,10, config.messageString)
-	
 
 def changeColor() :
 	pass
@@ -124,7 +132,7 @@ def runWork():
 	global config
 	while True:
 		iterate()
-		time.sleep(.003)
+		time.sleep(.005)
 		#time.sleep(random.random() * config.redrawSpeed)
 
 def iterate() :

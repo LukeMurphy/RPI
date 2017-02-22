b06b9a94ddfc3c51be095b6fca959a813644d615
Luke Murphy <web@lukelab.com>
2017-02-22 01:06:54 -0500

    ==> fills work ==> still need better way to make colored-greys

--- pieces/fills.py
+++ pieces/fills.py
@@ -20,15 +20,14 @@ class Fill :
 	height = 162
 	rows  = 1
 	cols = 1
-	lineWidth = 1
 	x = y = 0
-	currentColor = [0,0,0]
-	targetColor = [0,0,0]
-	rateOfChange = 0
+	currentColor = [80,80,80]
+	targetColor = [80,80,80]
 	rangeOfVals = [50,80]
-	redRange = [10,50]
-	greenRange = [10,20]
-	blueRange = [50,60]
+	rateOfChange = 0
+	redRange = [80,80]
+	greenRange = [80,80]
+	blueRange = [80,80]
 	speedFactor = 250
 
 	def __init__(self, config, x= 0 , y =0, width = 32, height = 32) :
@@ -37,14 +36,12 @@ class Fill :
 		self.y = y
 		self.height = height
 		self.width = width
-		val = random.uniform(self.rangeOfVals[0], self.rangeOfVals[1])
-		self.currentColor = [val,val,val]
-
-
+		#val = random.uniform(self.rangeOfVals[0], self.rangeOfVals[1])
+		#self.currentColor = [val,val,val]
+		self.speedFactor = self.config.speedFactor
 		self.currentColor = self.getRandomColorList()
 
 	def getRandomColorList(self) :
-
 		col = []
 		col.append(random.uniform(self.redRange[0],self.redRange[1]))
 		col.append(random.uniform(self.greenRange[0],self.greenRange[1]))
@@ -58,43 +55,93 @@ class Fill :
 		self.height = random.random() * 100 + 5
 
 	def make(self):
-		val = random.uniform(self.rangeOfVals[0], self.rangeOfVals[1])
-		self.targetColor = [val,val,val]
-		self.targetColor = self.getRandomColorList()
-		
-		self.displayTargetColor = tuple(int(i) for i in self.targetColor)
+		self.redRange = self.config.redRange
+		self.greenRange = self.config.greenRange
+		self.blueRange = self.config.blueRange
+
+		self.targetColor = tuple(int(i) for i in self.getRandomColorList())
 		self.displayCurrentColor = tuple(int(i) for i in self.currentColor)
 		
-		rate = random.random()/self.speedFactor
+		rate = (.01 + random.random()) / self.speedFactor
 		delta = tuple(self.targetColor[i] - self.currentColor[i] for i in range(0,3))
 		self.rateOfChange = tuple(i * rate for i in delta)
 
 
 	def update(self) :
-		color = []
-		for i in range(0,3):
-			color.append(self.currentColor[i] + self.rateOfChange[i]) 
 
-		self.currentColor = color
+		for i in range(0,3):
+			self.currentColor[i] += self.rateOfChange[i]
+
+		if(sum(self.displayCurrentColor) > 1000) :
+			self.currentColor[0] = self.targetColor[0]
+			self.currentColor[1] = self.targetColor[1]
+			self.currentColor[2] = self.targetColor[2]
+
+
+
+		if( self.displayCurrentColor[0] <= self.targetColor[0] and self.rateOfChange[0] < 0 ) or (self.displayCurrentColor[0] >= self.targetColor[0]  and  self.rateOfChange[0] > 0) or (self.displayCurrentColor[0] == self.targetColor[0]) :
+			self.rateOfChange = (0,self.rateOfChange[1],self.rateOfChange[2])
+			self.currentColor[0] = self.targetColor[0]
+
+		if ( self.displayCurrentColor[1] <= self.targetColor[1] and self.rateOfChange[1] < 0 ) or (self.displayCurrentColor[1] >= self.targetColor[1]  and  self.rateOfChange[1] > 0) or (self.displayCurrentColor[1] == self.targetColor[1]) :
+			self.rateOfChange = (self.rateOfChange[0],0,self.rateOfChange[2])
+			self.currentColor[1] = self.targetColor[1]
+
+		if ( self.displayCurrentColor[2] <= self.targetColor[0] and self.rateOfChange[2] < 0 ) or (self.displayCurrentColor[2] >= self.targetColor[2]  and  self.rateOfChange[2] > 0) or (self.displayCurrentColor[2] == self.targetColor[2]) :
+			self.rateOfChange = (self.rateOfChange[0],self.rateOfChange[1],0)
+			self.currentColor[2] = self.targetColor[2]
+
 		self.displayCurrentColor = tuple(int(i) for i in self.currentColor)
+
 		self.config.draw.rectangle((self.x, self.y, self.width + self.x, self.height + self.y), fill = self.displayCurrentColor)
-		
-		if(self.displayCurrentColor == self.displayTargetColor or self.displayCurrentColor >= (255,255,255) or self.displayCurrentColor <= (0,0,0)) :
-			#print("*************")
+
+		# ah, nifty Python tricks.....
+		if(all(v == 0 for v in self.rateOfChange)) :
 			self.make()
 
 
 
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+def setColor(clr="blue", dominant=False) :
+	print(clr)
+	global config
+	config.clr = clr
+	if (clr == "red") :
+		config.redRange = [config.redModel[0], config.redModel[1]] #if dominant == False else [10,10]
+		config.greenRange = [config.redModel[2], config.redModel[3]]# if dominant == False else [0,0]
+		config.blueRange = [config.redModel[4], config.redModel[5]] #if dominant == False else [0,0]
+	if (clr == "green") :
+		config.redRange = [config.greenModel[0], config.greenModel[1]] #if dominant == False else [10,10]
+		config.greenRange = [config.greenModel[2], config.greenModel[3]]# if dominant == False else [8,8]
+		config.blueRange = [config.greenModel[4], config.greenModel[5]] #if dominant == False else [0,0]
+	if (clr == "blue") :
+		config.redRange = [config.blueModel[0], config.blueModel[1]] #if dominant == False else [0,0]
+		config.greenRange = [config.blueModel[2], config.blueModel[3]] #if dominant == False else [0,0]
+		config.blueRange = [config.blueModel[4], config.blueModel[5]] #if dominant == False else [10,10]
+
 
 def main(run = True) :
 	global config
-	config.lineWidth = int(workConfig.get("fills", 'lineWidth'))
-	config.pulseSpeed = float(workConfig.get("fills", 'pulseSpeed'))
+	config.redrawRate = float(workConfig.get("fills", 'redrawRate'))
 	config.mode = (workConfig.get("fills", 'mode'))
-	config.countLimit = int(workConfig.get("fills", 'countLimit'))
 
+	config.speedFactor = float(workConfig.get("fills", 'speedFactor'))
+	config.blurLevel = int(workConfig.get("fills", 'blurLevel'))
+
+	config.redModel = ((workConfig.get("fills", 'redModel')).split(','))
+	config.redModel = map(lambda x: int(x), config.redModel)
+	config.greenModel = ((workConfig.get("fills", 'greenModel')).split(','))
+	config.greenModel = map(lambda x: int(x), config.greenModel)
+	config.blueModel = ((workConfig.get("fills", 'blueModel')).split(','))
+	config.blueModel = map(lambda x: int(x), config.blueModel)
+	config.models =["red","green","blue"]
+	config.clr = "blue"
+	config.transitioning = False
+
+	c = int(random.uniform(0, 3))
+	c = 2
+	setColor(config.models[c])
 
 	# reseting render image size
 	config.renderImage = Image.new("RGBA", (config.actualScreenWidth , 32))
@@ -102,9 +149,7 @@ def main(run = True) :
 	config.draw  = ImageDraw.Draw(config.image)
 	config.id = config.image.im.id
 
-
 	config.fill = []
-
 	for col in range (0,config.cols) :
 		for row in range (0,config.rows) :
 			f  = Fill(config, col * 32, row * 32)
@@ -120,7 +165,7 @@ def runWork():
 	global config
 	while True:
 		iterate()
-		time.sleep(config.pulseSpeed)
+		time.sleep(config.redrawRate)
 
 
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
@@ -133,9 +178,28 @@ def iterate() :
 	for i in range (0,numBlocks) :
 		config.fill[i].update()
 
-	im = config.image.filter(ImageFilter.GaussianBlur(12))
+	im = config.image.filter(ImageFilter.GaussianBlur(config.blurLevel))
 	#im2 = im.filter(ImageFilter.MinFilter(3))
 	config.render(im, 0, 0,config.screenWidth,config.screenHeight)
+
+
+	# Change dominant color
+	if(random.random() < .0005 and config.fill[0].speedFactor != 20) :
+		c = int(random.uniform(0, 3))
+		if config.models[c] != config.clr :
+			setColor(config.models[c], True)
+			config.transitioning = True
+			for i in range (0,numBlocks) :
+				config.fill[i].speedFactor = 20
+				config.fill[i].make()
+
+	# After the transition is complete, normal slow variations
+	if(config.fill[0].speedFactor == 20 and random.random() < .01 and config.transitioning == True) :
+		setColor(config.clr)
+		config.transitioning = False
+		for i in range (0,numBlocks) :
+			config.fill[i].speedFactor = config.speedFactor
+			config.fill[i].make()
 
 
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''

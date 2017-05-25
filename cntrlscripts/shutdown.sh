1318c69443e7960269b12e0fd99955e019b47145
Luke Murphy <web@lukelab.com>
2017-05-25 17:49:43 -0400

    ==> config changes

--- cntrlscripts/shutdown.sh
+++ cntrlscripts/shutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Shutting Down Python scripts"
-ps -eaf | pgrep python | xargs sudo kill;
+ps -eaf | pgrep python | xargs sudo kill -9;
 sleep 1
 echo "Doing shutdown / halt"
 sudo shutdown -h now

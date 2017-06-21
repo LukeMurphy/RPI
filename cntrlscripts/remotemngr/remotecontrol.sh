d8e23bde046e24386778adcc730af7c758b48461
Luke Murphy <luke@lukelab.com>
2017-06-21 08:17:15 -0400

    ==> adjusting

--- cntrlscripts/remotemngr/remotecontrol.sh
+++ cntrlscripts/remotemngr/remotecontrol.sh
@@ -1,43 +1,43 @@
+#!/bin/sh
 #!/bin/bash
 # Remote reader
-path="/Users/lamshell/Documents/Dev/RPI/"
+path="/home/lukr/Documents/RPI/"
 configGroup="configs/p4-4x4/"
 machine="daemon4"
 
 remotevalue=$(curl -A "Mozilla/5.0 (Windows NT 5.1; rv:21.0) Gecko/20130401 Firefox/21" "http://www.lukelab.com/projects/rpi-controls/daemon4-status.cfg")
-localvalue=$(cat localvalue.cfg)
+localvalue=$(cat /home/lukr/Documents/RPI/cntrlscripts/remotemngr/localvalue.cfg)
 echo $remotevalue
 echo $localvalue
-if [ $remotevalue != $localvalue ] 
+if [ $remotevalue != $localvalue ]
 then
-	echo "NOT THE SAME"
-	echo -n $remotevalue > localvalue.cfg
+    echo "NOT THE SAME"
+    echo -n $remotevalue > /home/lukr/Documents/RPI/cntrlscripts/remotemngr/localvalue.cfg
 
-	ps -ef | pgrep Python | xargs sudo kill -9;
+    ps -ef | pgrep python | xargs sudo kill -9;
 
-	if [ $remotevalue == "fludd" ]
-	then
-		config="fludd.cfg"
-	fi
+    if test $remotevalue = "fludd"
+    then
+        config="fludd.cfg"
+    fi
 
-	if [ $remotevalue == "marquee" ]
-	then
-		config="marquee.cfg"
-	fi
+    if test  $remotevalue = "marquee"
+    then
+        config="marquee.cfg"
+    fi
 
-	if [ $remotevalue == "repaint" ]
-	then
-		config="repaint.cfg"
-	fi
+    if test  $remotevalue = "repaint"
+    then
+        config="repaint.cfg"
+    fi
 
-	if [ $remotevalue == "flames" ]
-	then
-		config="img-hub.cfg"
-	fi
+    if test  $remotevalue = "flames"
+    then
+        config="img-hub.cfg"
+    fi
 
-	execString=$path"player.py "$machine" "$path" "$path$configGroup$config
-	echo $execString
+    execString=$path"player.py "$machine" "$path" "$path$configGroup$config
+    echo $execString
 
-	python $execString&
- 
+    DISPLAY=:0 python $execString&
 fi

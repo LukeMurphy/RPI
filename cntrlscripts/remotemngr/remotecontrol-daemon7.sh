9b0e8411f73bd5223fcef41d5a16ffcc4e3ad474
Luke Murphy <web@lukelab.com>
2018-02-11 10:57:39 -0500

    ==> fixes for new launch args

--- cntrlscripts/remotemngr/remotecontrol-daemon7.sh
+++ cntrlscripts/remotemngr/remotecontrol-daemon7.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Changes based on machine setup
 path="/home/daemon7/Documents/RPI/"
-configGroup="configs/p4-4x4/"
+configGroup="/p4-4x4/"
 machine="daemon7"
 pieceFileName="http://www.lukelab.com/projects/rpi-controls/daemon7-status.cfg"
 brightnessFile="http://www.lukelab.com/projects/rpi-controls/daemon7-controlstatus.cfg"
--- cntrlscripts/remotemngr/remotecontrol-daemon8.sh
+++ cntrlscripts/remotemngr/remotecontrol-daemon8.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Changes based on machine setup
 path="/home/daemon8/Documents/RPI/"
-configGroup="configs/p4-4x4/"
+configGroup="/p4-4x4/"
 machine="daemon8"
 pieceFileName="http://www.lukelab.com/projects/rpi-controls/daemon8-status.cfg"
 brightnessFile="http://www.lukelab.com/projects/rpi-controls/daemon8-controlstatus.cfg"

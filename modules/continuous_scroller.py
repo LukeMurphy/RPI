b773c34f507c9781409a64c1a44e87fb51866ab1
Luke Murphy <web@lukelab.com>
2018-02-12 20:26:18 -0500

    ==> preparing better scrolling mechanisms

---
+++ modules/continuous_scroller.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+#import modules
+# ################################################### #
+import os, sys, getopt, time, random, math, datetime, textwrap
+import ConfigParser, io
+import importlib 
+import numpy
+import threading
+import resource
+from collections import OrderedDict
+from PIL import ImageFont, Image, ImageDraw, ImageOps, ImageChops, ImageEnhance
+from modules import colorutils, coloroverlay
+
+global thrd, config
+
+class ScrollObject :
+
+	
+	canvasWidth = 330
+	canvasHeight = 64
+	windowWidth = 320
+	windowHeight = 224
+	xPos = 0
+	yPos = 0
+	xSpeed = 2
+	ySpeed = 0
+	xOffset = 0
+	yOffset = 0
+	bgBackGroundColor = (200,0,0)
+
+
+
+	def __init__(self, direction="right-left") :
+
+		self.canvas = Image.new("RGBA", (self.canvasWidth, self.canvasHeight))
+		self.canvasDraw = ImageDraw.Draw(self.canvas)
+
+		self.bg1 = Image.new("RGBA", (self.canvasWidth, self.canvasHeight))
+		self.bg1Draw = ImageDraw.Draw(self.bg1)
+		#self.bg1Draw.rectangle((0,0,self.canvasWidth, self.canvasHeight), fill = self.bgBackGroundColor)
+
+		
+		self.bg2 = Image.new("RGBA", (self.canvasWidth, self.canvasHeight))
+		self.bg2Draw = ImageDraw.Draw(self.bg2)
+		#self.bg2Draw.rectangle((0,0,self.canvasWidth, self.canvasHeight), fill = self.bgBackGroundColor)
+
+		self.leadImage = self.bg1
+		self.followImage = self.bg2
+
+		self.bg1Draw.rectangle((0,0,100,50), fill=(0,200,0))
+		self.bg1Draw.rectangle((self.canvasWidth - 20,0,self.canvasWidth,50), fill=(0,200,200))
+
+		self.bg2Draw.rectangle((0,0,100,50), fill=(0,00,200))
+		self.bg2Draw.rectangle((self.canvasWidth - 30,0,self.canvasWidth,50), fill=(200,20,200))
+
+		self.canvas.paste(self.bg1)
+		self.canvas.paste(self.bg2,(self.canvasWidth, 0))
+
+
+		print("Scroller Initiated")
+
+
+	def scroll(self):
+
+		self.xPos += self.xSpeed
+		self.yPos += self.ySpeed
+		lead = self.leadImage
+		swap = False
+
+		## Just right-left scrolling for now ...
+		#self.canvasDraw.rectangle((0,0,self.canvasWidth,self.canvasHeight), fill  = (0,0,0,10))
+		self.canvas.paste(self.leadImage, (self.xPos ,self.yPos))
+		if (self.xSpeed > 0) : 
+			self.canvas.paste(self.followImage, (self.xPos - self.canvasWidth,self.yPos ))
+		else :
+			self.canvas.paste(self.followImage, (self.xPos + self.canvasWidth,self.yPos ))
+
+		if (self.xPos > 1 * self.canvasWidth and self.xSpeed > 0) : 
+			self.canvas.paste(self.leadImage, ( -1 * self.canvasWidth, self.yPos))
+			#makeBackGround(leadBGDraw)
+			swap = True
+
+		if (self.xPos < -1 * self.canvasWidth and self.xSpeed < 0) : 
+			self.canvas.paste(self.leadImage, ( 1 * self.canvasWidth, self.yPos))
+			#makeBackGround(leadBGDraw)
+			swap = True
+
+		if(swap == True):
+			self.leadImage = self.followImage
+			self.followImage = lead
+			self.xPos = 0
+
+		self.render()
+
+	def render(self) :
+		pass
+
+
---
+++ pieces/iconscroller.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+#import modules
+# ################################################### #
+import os, sys, getopt, time, random, math, datetime, textwrap
+import ConfigParser, io
+import importlib 
+import numpy
+import threading
+import resource
+from collections import OrderedDict
+from PIL import ImageFont, Image, ImageDraw, ImageOps, ImageChops, ImageEnhance
+from modules import colorutils, coloroverlay, continuous_scroller
+
+global thrd, config
+
+def init() :
+	global config
+	config.redrawSpeed  = float(workConfig.get("scroller", 'redrawSpeed')) 
+
+	config.windowWidth  = float(workConfig.get("displayconfig", 'windowWidth')) 
+	config.windowHeight  = float(workConfig.get("displayconfig", 'windowHeight')) 
+
+	config.xOffset = int(workConfig.get("scroller", 'xOffset')) 
+	config.yOffset = int(workConfig.get("scroller", 'yOffset')) 
+
+	config.displayRows = int(workConfig.get("scroller", 'displayRows'))
+	config.displayCols = int(workConfig.get("scroller", 'displayCols'))
+
+	config.canvasImage = Image.new("RGBA", (config.canvasWidth, config.canvasHeight))
+	config.canvasImageDraw = ImageDraw.Draw(config.canvasImage)
+
+	config.workImage = Image.new("RGBA", (config.canvasWidth, config.canvasHeight))
+	config.workImageDraw = ImageDraw.Draw(config.workImage)
+
+	config.scroller1 = continuous_scroller.ScrollObject()
+
+	config.scroller2 = continuous_scroller.ScrollObject()
+	config.scroller2.xSpeed = -4
+
+
+def makeArrows(drawRef, n =1) :
+
+	drawRef = config.canvasImageDraw
+	rows = config.patternRows * 2
+	cols = config.patternCols * 2
+
+	xDiv = config.canvasWidth / cols 
+	yDiv = config.canvasHeight / rows 
+
+	xStart = config.canvasWidth / 2
+	yStart = config.canvasHeight / 2
+
+	arrowLength = cols
+	blade = cols / 3
+	bufferDistance = cols - 5
+
+	clr  = (200,0,0)
+
+	drawRef.rectangle((0,0,config.canvasWidth, config.canvasHeight), fill = config.bgBackGroundColor)
+
+	for c in range (0, cols) : 
+		yArrowEnd = 0 #yStart + arrowLength
+		xArrowEnd = xStart + arrowLength
+
+		# the horizontal
+		drawRef.line((xStart, yStart, xStart, yArrowEnd), fill = clr, width = config.lineThickness)
+		# the blades
+		xDisplace = 0 #blade * math.tan(math.pi/4)
+		yDisplace = blade * math.tan(math.pi/4)
+
+		#drawRef.line((xStart - xDisplace, yArrowEnd - blade, xStart, yArrowEnd), fill = clr, width = config.lineThickness)
+		#drawRef.line((xStart + xDisplace, yArrowEnd - blade , xStart, yArrowEnd), fill = clr, width = config.lineThickness)
+		drawRef.line((xStart - xDisplace, yArrowEnd - blade, xStart, yArrowEnd), fill = clr, width = config.lineThickness)
+		drawRef.line((xStart + xDisplace, yArrowEnd - blade , xStart, yArrowEnd), fill = clr, width = config.lineThickness)
+	
+		#yStart += arrowLength + bufferDistance
+		xStart += arrowLength + bufferDistance
+
+def callBack() :
+	global config
+	pass
+
+def runWork():
+	global config
+	while True:
+		iterate()
+		time.sleep(config.redrawSpeed)
+
+def iterate() :
+	global config
+
+	config.workImageDraw.rectangle((0,0,config.windowWidth,config.windowHeight), fill  = (0,0,0))
+	
+	config.scroller1.scroll()
+	config.workImage.paste(config.scroller1.canvas, (0,0), config.scroller1.canvas)
+
+	config.scroller2.scroll()
+	config.workImage.paste(config.scroller2.canvas, (0,0), config.scroller2.canvas)
+
+	config.render(config.workImage, 0,0)
+
+def main(run = True) :
+	global config, threads, thrd
+	init()
+	
+	if(run) : runWork()
+
+### Kick off .......
+if __name__ == "__main__":
+	__main__()

5056506a5bf37e485c32f3097854ad1095611e30
Luke Murphy <web@lukelab.com>
2018-04-24 00:10:41 -0400

    ==> production congigs testing

--- cntrlscripts/run2.sh
+++ cntrlscripts/run2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ps -ef | pgrep Python | xargs sudo kill -9;
+ps -ef | pgrep python | xargs sudo kill -9;
 #sudo python /Users/lamshell/Documents/Dev/RPI/player.py studio-mac ./ configs/"$@".cfg & 
 sudo python3 player.py -mname studio-mac -path ./ -cfg "$@.cfg"& 

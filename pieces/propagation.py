6fb82ac7894958410e40eafaf366dc007cfcf903
Luke Murphy <web@lukelab.com>
2018-07-08 18:04:38 -0400

    ==> updates to colorOverlay and propagation for more control

--- pieces/propagation.py
+++ pieces/propagation.py
@@ -210,18 +210,20 @@ def redrawGrid2():
 				if targetUnit.score == 1 :
 					score +=1
 
-		if u.score == 1 and score < 2 :
-			u.score = 0
+		if u.score == 1 :
+			if score < config.underPopulationThreshold :
+				u.score = 0
 
-		## Should be > 3
-		if u.score == 1 and score > config.overCrowdingThreshold :
-			u.score = 0
+			## Should be > 3
+			if score > config.overCrowdingThreshold :
+				u.score = 0
 
-		if u.score == 1 and score > 1 and score < 4 :
-			u.score = 1
+			if score > 1 and score < config.dieThreshold  :
+				u.score = 1
 
-		if u.score == 0 and score == config.liveThreshold  :
-			u.score = 1
+		if u.score == 0 :
+			if score == config.liveThreshold  :
+				u.score = 1
 
 
 		if u.score == 1 :
@@ -262,8 +264,11 @@ def main(run = True) :
 	config.coordinatedColorChange = False
 	config.propagationProbability = float(workConfig.get("signage", 'propagationProbability'))
 	config.doneThreshold = float(workConfig.get("signage", 'doneThreshold'))
+	
 	config.overCrowdingThreshold = int(workConfig.get("signage","overCrowdingThreshold"))
+	config.underPopulationThreshold = int(workConfig.get("signage","underPopulationThreshold"))
 	config.liveThreshold = int(workConfig.get("signage","liveThreshold"))
+	config.dieThreshold = int(workConfig.get("signage","dieThreshold"))
 
 
 	config.timeTrigger = workConfig.getboolean("signage", 'timeTrigger')

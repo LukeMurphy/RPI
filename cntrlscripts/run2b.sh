1d8f5b16bf8c64b367e622d5b68caa072f603545
Luke Murphy <web@lukelab.com>
2018-11-27 15:41:20 -0500

    ==> new refinemantes

---
+++ cntrlscripts/run2b.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#sudo python /Users/lamshell/Documents/Dev/RPI/player.py studio-mac ./ configs/"$@".cfg & 
+if [ "$2" == "b" ]; then 
+echo
+else
+ps -ef | pgrep Python 
+#| xargs sudo kill -9; 
+fi
+echo
+echo 
+sudo python3 player.py -mname studio-mac -path ./ -cfg "$1.cfg"& 

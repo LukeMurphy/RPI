7351ec12529a49ae68b92858ddff1f0224c64982
Luke Murphy <web@lukelab.com>
2019-01-09 11:29:17 -0500

    refinementss - trying Flag

--- modules/badpixels.py
+++ modules/badpixels.py
@@ -5,70 +5,88 @@ import math
 
 blankPixels = []
 counterPixels = []
+size = []
 
 colsRange = (2,20)
 rowsRange = (2,20)
-
-size = (160,160)
 
 totalCounterWidth = 0
 numberOfDeadPixels = 10
 probabilityOfBlockBlanks = .9
 
-def setBlanks() : return None
+def setBlanks() : 
+	return None
+
 
 def setBlanksOnImage() :
 	#print("Setting Blanks")
-	global config,blankPixels, size, numberOfDeadPixels, probabilityOfBlockBlanks
+	global config,blankPixels, sizeTarget, numberOfDeadPixels, probabilityOfBlockBlanks
 	global colsRange, rowsRange
 	blankPixels = []
 	count = 0
-	size = config.renderImageFull.size
-	# scatter horizontally
+	
+
+	# scatter 
 	for n in range (0, numberOfDeadPixels) :
-		x = int(random.random()*size[0])
-		y = int(random.random()*size[1])
+		x = round(random.random()*sizeTarget[0])
+		y = round(random.random()*sizeTarget[1])
 		blankPixels.append((x,y))
 		if(random.random() > probabilityOfBlockBlanks):
-			cols = int(random.uniform(colsRange[0],colsRange[1]))
-			rows = int(random.uniform(rowsRange[0],rowsRange[1]))
+			cols = round(random.uniform(colsRange[0],colsRange[1]))
+			rows = round(random.uniform(rowsRange[0],rowsRange[1]))
 
 			for ii in range(0,rows):
 				blankPixels.append((x,y + ii))
 				for i in range (0,cols) :
 					blankPixels.append((x+i,y + ii))
+
+
 
 def setBlanksOnScreen() :
 	#print("Setting Blanks")
 	global config,blankPixels,numberOfDeadPixels, probabilityOfBlockBlanks
-	global colsRange, rowsRange
+	global colsRange, rowsRange, sizeTarget
 	blankPixels = []
 	count = 0
+	
 	# scatter horizontally
 	for n in range (0, numberOfDeadPixels) :
-		x = int(random.random()*config.actualScreenWidth)
-		y = int(random.random()*48)
+		x = round(random.random()*config.screenWidth)
+		y = round(random.random()*config.screenHeight)
 		blankPixels.append((x,y))
 		if(random.random() > probabilityOfBlockBlanks):
-			cols = int(random.uniform(colsRange[0],colsRange[1]))
-			rows = int(random.uniform(rowsRange[0],rowsRange[1]))
+			cols = round(random.uniform(colsRange[0],colsRange[1]))
+			rows = round(random.uniform(rowsRange[0],rowsRange[1]))
 
 			for ii in range(0,rows):
 				blankPixels.append((x,y + ii))
 				for i in range (0,cols) :
 					blankPixels.append((x+i,y + ii))
-		
+
+
+
 def drawBlanks(target=None, direct = True) :
-	global config, blankPixels,drawBlanksFlag, size
+	global config, blankPixels,drawBlanksFlag, sizeTarget
 	global colsRange, rowsRange
-	if(target==None) : target = config.renderImageFull
-	if (len(blankPixels) == 0): setBlanks()
+	if(target == None) : 
+		target = config.renderImageFull
+		sizeTarget = list(config.renderImageFull)
+
 	count = 0
-	blankNum=len(blankPixels)
+	blankNum = len(blankPixels)
 
 	for n in range (0, blankNum) :
 		if(direct) :
 			config.matrix.SetPixel(blankPixels[n][0],blankPixels[n][1],0,0,0)
 		else :
-			if(blankPixels[n][0] < size[0] and blankPixels[n][1] < size[1]) :
+			if(blankPixels[n][0] < sizeTarget[0] and blankPixels[n][1] < sizeTarget[1]) :
 				target.putpixel((blankPixels[n][0],blankPixels[n][1]),(0,0,0))
+
+
+
+
+
+
+
+
+
--- pieces/flag.py
+++ pieces/flag.py
@@ -15,6 +15,9 @@ def main(run=True):
 	global config, workConfig
 
 	config.delay = float(workConfig.get("flag", 'delay'))
+	config.whiteBrightness = float(workConfig.get("flag", 'whiteBrightness'))
+	config.starBrightness = float(workConfig.get("flag", 'starBrightness'))
+	config.colorBrightness = float(workConfig.get("flag", 'colorBrightness'))
 
 	config.image = Image.new("RGBA", (config.screenWidth  , config.screenHeight))
 	config.draw = ImageDraw.Draw(config.image)
@@ -31,12 +34,25 @@ def main(run=True):
 	config.colOverlay.steps = 50
 
 
-	config.actualScreenWidth = 256
+	config.stripeHeight = config.canvasHeight / 13
+	config.redVal = (120,0,0,255)
+	config.whtVal = (220,220,220,255)
+	config.starWhtVal = (220,220,220,255)
+	config.blueVal = (0,0,120,255)
+
+	config.starHDis = 0.063 * config.canvasHeight
+	config.starVDis = 0.054 * config.canvasHeight
+	config.starDiam = 0.036 * config.canvasHeight
+	config.blueWidth = config.canvasHeight * 0.76 * .8
+	config.blueHeight = config.canvasHeight * 0.5385
+
+	config.starPointsAngle = 2*math.pi/5
+	config.radius = config.starDiam/2
 
 
-	badpixels.numberOfDeadPixels = 10
-	badpixels.size = (256,256)
+	badpixels.numberOfDeadPixels = int(workConfig.get("flag", 'numberOfDeadPixels'))
 	badpixels.config = config
+	badpixels.sizeTarget = list(config.canvasImage.size)
 	badpixels.setBlanksOnScreen() 
 	
 
@@ -46,43 +62,33 @@ def main(run=True):
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 def drawRects() :
-	stripeHeight = config.canvasHeight / 13
-	redVal = (120,0,0,255)
-	whtVal = (220,220,220,255)
-	starWhtVal = (220,220,220,255)
-	blueVal = (0,0,120,255)
 
-	starHDis = 0.063 * config.canvasHeight
-	starVDis = 0.054 * config.canvasHeight
-	starDiam = 0.036 * config.canvasHeight
-	blueWidth = config.canvasHeight * 0.76 * .8
-	blueHeight = config.canvasHeight * 0.5385
-
-	starPointsAngle = 2*math.pi/5
-	radius = starDiam/2
-
+	starWhtVal = tuple(round(c * config.starBrightness) for c in config.starWhtVal)
+	blueVal = tuple(round(c * config.colorBrightness) for c in config.blueVal)
+	redVal = tuple(round(c * config.colorBrightness) for c in config.redVal)
+	whtVal = tuple(round(c * config.whiteBrightness) for c in config.whtVal)
 
 	for s in range(0,13) :
 		fillVal = redVal
 		if s % 2 > 0 :
 			fillVal = whtVal
-		config.canvasDraw.rectangle((0, stripeHeight * s, config.canvasWidth, stripeHeight * s + stripeHeight), fill=fillVal)
+		config.canvasDraw.rectangle((0, config.stripeHeight * s, config.canvasWidth, config.stripeHeight * s + config.stripeHeight), fill=fillVal)
 
-	config.canvasDraw.rectangle((0,0,blueWidth,blueHeight), fill=blueVal)
+	config.canvasDraw.rectangle((0,0,config.blueWidth,config.blueHeight), fill=blueVal)
 
 	for r in range(0,7) :
 		starsNum = 6
 		offset = 0
 		if r%2 > 0 :
 			starsNum = 5
-			offset = starHDis
+			offset = config.starHDis
 		for s in range(0, starsNum):
-			xPos = offset + starHDis/1.5 + (starHDis + starDiam) * s
-			yPos = starVDis + (starVDis/1.5 + starDiam) * r
+			xPos = offset + config.starHDis/1.5 + (config.starHDis + config.starDiam) * s
+			yPos = config.starVDis + (config.starVDis/1.5 + config.starDiam) * r
 			starPoints = []
 			for i in range(0,5) :
-				xP = xPos + radius * math.cos(i * starPointsAngle)
-				yP = yPos + radius * math.sin(i * starPointsAngle)
+				xP = xPos + config.radius * math.cos(i * config.starPointsAngle)
+				yP = yPos + config.radius * math.sin(i * config.starPointsAngle)
 				starPoints.append((xP,yP))
 			#config.canvasDraw.rectangle(xPos, yPos,xPos + starDiam,yPos + starDiam), fill=whtVal)
 			#config.canvasDraw.chord((xPos, yPos,xPos + starDiam,yPos + starDiam),0,360, fill=whtVal)

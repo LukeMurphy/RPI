4e86d9d44cc83f4a891716c898f8c286e756168a
Luke Murphy <web@lukelab.com>
2019-02-06 00:17:18 -0500

    == tried to make a conversion funnel or vortex, made a kind of icon-angel harbinger - p4-5x8 - test

--- pieces/compositions.py
+++ pieces/compositions.py
@@ -160,7 +160,7 @@ def restartDrawing() :
 	if random.random() < config.cleanSlateProbability or config.firstRun == True :
 		#grayLevel = round(random.uniform(20,70))
 		#config.bgColor = (grayLevel,grayLevel,grayLevel)
-		config.bgColor = colorutils.getRandomColorHSV(0,360, .0,.15, .1,.4)
+		config.bgColor = colorutils.getRandomColorHSV(0,360, .0,.5, .1,.4)
 		config.draw.rectangle((0,0,config.imageWidth, config.imageHeight), fill=config.bgColor)
 		config.firstRun = False
 	drawCompositions()

93a884fd0ff28435910f6db239f8e4554fa7252e
Luke Murphy <web@lukelab.com>
2019-05-31 17:09:13 -0400

    --> P10 branch lean Configs

--- cntrlscripts/demo.py
+++ cntrlscripts/demo.py
@@ -44,6 +44,7 @@ actionDict1 = [
 	{"* P10 Branch - Algoflames-b" :'p10/algoflames-1b.cfg'},
 	{"* P10 Branch - Quilt" :'p10/branch-quilt-1.cfg'},
 	{"* P10 Branch - Lines On" :'p10/collage-lines-1.cfg'},
+	{"* P10 Branch - Tourmaline" :'p10/collage-tourmaline.cfg'},
 
 
 

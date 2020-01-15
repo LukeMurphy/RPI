0c7fc3b2fe032cada5d32881f1217e832271a752
Luke Murphy <web@lukelab.com>
2020-01-15 16:14:40 -0500

    ==> updates adafruit hat driver etc

--- cntrlscripts/stest.py
+++ cntrlscripts/stest.py
@@ -8,26 +8,27 @@ import time
 
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
 
 # GPIO 18 set up as input. It is pulled up to stop false signals
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+def callBack(arg):
+	print("shutting down CALLLED!")
+	try:
+		print("shutdown call")
+		# os.system("/home/pi/Documents/RPI/cntrlscripts/shutdown.sh")
+		subprocess.call(["/home/pi/Documents/RPI/cntrlscripts/shutdown.sh"])
+	except OSError:
+		print(OSError)
+		subprocess.call(["/home/pi/Documents/RPI/cntrlscripts/shutdown.sh"])
+		# os.system("/home/pi/RPI/cntrlscripts/shutdown.sh")
 
 def __main__():
 	print("Started testing GPIO for off signal")
-	try:
-		GPIO.wait_for_edge(18, GPIO.FALLING)
-		print("shutting down!")
-		time.sleep(0.1)
-		try:
-			print("shutdown call")
-			# os.system("/home/pi/Documents/RPI/cntrlscripts/shutdown.sh")
-			subprocess.call(["/home/pi/Documents/RPI/cntrlscripts/shutdown.sh"])
-		except OSError:
-			print(OSError)
-			subprocess.call(["/home/pi/RPI/cntrlscripts/shutdown.sh"])
-			# os.system("/home/pi/RPI/cntrlscripts/shutdown.sh")
-	except KeyboardInterrupt:
-		GPIO.cleanup()  # clean up GPIO on CTRL+C exit
-	GPIO.cleanup()  # clean up GPIO on normal exit
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(18, GPIO.FALLING, callback=callBack, bouncetime=10)

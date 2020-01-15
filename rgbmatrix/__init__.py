f8a433bff5ec27101540fe5be1975dd633be6f32
Luke Murphy <web@lukelab.com>
2020-01-15 15:25:31 -0500

    ==> updates adafruit hat driver etc

---
+++ rgbmatrix/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+print("Loading from rgbmatrix folder")
+
+__version__ = "0.0.1"
+__author__ = "Christoph Friedrich <christoph.friedrich@vonaffenfels.de>"
+
+from .core import RGBMatrix, FrameCanvas, RGBMatrixOptions

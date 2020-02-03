7d1e9c3029d04116a80252212d43ab4d663764d4
Luke Murphy <web@lukelab.com>
2020-02-03 11:00:43 -0500

    ==> adding dots

--- pieces/singletons/spots.py
+++ pieces/singletons/spots.py
@@ -156,8 +156,8 @@ class Spot :
 					dotVariation = random.random() * self.dotVariation
 				for i in range(0,3) :
 					d = Dot()
-					d.xOffSet = (self.dotSize + self.packing) * c + xVariation * (i-1) * 2
-					d.yOffSet = (self.dotSize + self.packing) * r + yVariation
+					d.xOffSet = (self.dotSize + self.packing) * c + xVariation * (i-2)*5
+					d.yOffSet = (self.dotSize + self.packing) * r + yVariation * (2-i)*5
 					d.fillColor = self.clrs[i]
 					if self.dotVariationByColor == True :
 						dotVariation = random.random() * self.dotVariation
@@ -182,9 +182,9 @@ class Spot :
 			for i in range(0,3):
 				self.spotsArray[n][i].drawOval()
 				#self.spotsArray[i].xPos += i
-				self.workImage = ImageChops.add_modulo(self.workImage , self.spotsArray[n][2].workImage)
-				self.workImage = ImageChops.add_modulo(self.workImage , self.spotsArray[n][0].workImage)
-				self.workImage = ImageChops.add_modulo(self.workImage , self.spotsArray[n][1].workImage)
+				self.workImage = ImageChops.add(self.workImage , self.spotsArray[n][2].workImage)
+				self.workImage = ImageChops.add(self.workImage , self.spotsArray[n][0].workImage)
+				self.workImage = ImageChops.add(self.workImage , self.spotsArray[n][1].workImage)
 
 		self.workImage = self.workImage.filter(ImageFilter.GaussianBlur(radius=self.blurRadius))
 

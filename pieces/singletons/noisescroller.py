8778a06898280c0515758b7c04c595ecea8a9a77
Luke Murphy <web@lukelab.com>
2021-06-12 11:21:26 -0300

    variations

--- pieces/singletons/noisescroller.py
+++ pieces/singletons/noisescroller.py
@@ -17,7 +17,57 @@ colorutils.brightness = 1
 
 
 def reDraw():
-	rings()
+	if config.function == "wavey" :
+		wavey()
+	if config.function == "ringLines" :
+		ringLines()
+	if config.function == "rings" :
+		rings()
+	if config.function == "waves" :
+		waves()
+
+
+def ringLines():
+	global config
+	config.draw.rectangle((0,0,500,500), fill=config.bgColor)
+	gDelta = 1 + 1/config.rgbSplitFactor
+	bDelta = 1 + 1/config.rgbSplitFactor + 1/config.rgbSplitFactor
+
+	lastx = [config.xOffset,config.xOffset,config.xOffset]
+	lasty = [config.yOffset,config.yOffset,config.yOffset]
+
+
+	for row in range(0,config.numRings):
+		points = config.pointsMin + row * config.pointsMin
+		rads = 2 * math.pi / points
+		ra = config.radiusMin * row + config.radiusMin
+		for col in range(0,points):
+			x = math.cos(col * rads) * ra + config.xOffset
+			y = math.sin(col * rads) * ra + config.yOffset
+
+			yChange1 = noise.pnoise2(x/config.rowFactor, (y + config.scroll)/config.colFactor/1, 1) * config.amplitude + row
+			yChange2 = noise.pnoise2(x/config.rowFactor, (y + config.scroll)/config.colFactor/gDelta, 1) * config.amplitude + row
+			yChange3 = noise.pnoise2(x/config.rowFactor, (y + config.scroll)/config.colFactor/bDelta, 1) * config.amplitude + row
+			
+			if config.drawOptimize == True :
+				doDraw =  False
+			else :
+				doDraw = True 
+
+			if x > 0  and x < config.canvasWidth-config.xOffset and (y + yChange1)  > 0 and (y + yChange1) <  config.canvasHeight:
+				doDraw = True
+
+			if doDraw == True:
+
+				config.draw.line((lastx[0],lasty[0], x, y + yChange1), fill=(255,0,10,250))
+				config.draw.line((lastx[0],lasty[0], x, y + yChange2), fill=(200,120,10,255))
+				config.draw.line((lastx[0],lasty[0], x, y + yChange3), fill=(52,0,250,155))
+				lastx = [x,x,x]
+				lasty = [y + yChange1,y + yChange2,y + yChange3]
+
+		#octv += 1
+	config.scroll += 1
+
 
 def rings():
 	global config
@@ -57,19 +107,48 @@ def rings():
 		#octv += 1
 	config.scroll += 1
 
+def wavey():
+	global config
+	config.draw.rectangle((0,0,500,500), fill=config.bgColor)
+	octv = 1
+	lastx = [0,0,0]
+	for col in range(0,config.canvasWidth*1,2):
+		lasty = [0,0,0]
+		for row in range(0,config.canvasHeight,8):
+			y = row
+			x = round(noise.pnoise2((col +config.scroll)/config.colFactor/3, (y )/config.rowFactor/1, 1) * config.amplitude + col)
+
+			r = 255
+			g = round(math.sin((col/config.rowFactor)+.1) * 200)
+			#g = round(math.sin((x/config.rowFactor)+.1) * 200)
+			b = 50
+			#config.draw.rectangle((x, y, x+1, y+1), fill=(r,g,b,150))
+			if col != 0 :
+				config.draw.line((lastx[0],lasty[0],x,y), fill = (r,g,b,150))
+			lastx = [x,x,x]
+			lasty = [y,y,y]
+		#octv += 1
+	config.scroll += 1.8
+
 def waves():
 	global config
 	config.draw.rectangle((0,0,500,500), fill=config.bgColor)
 	octv = 1
-	for row in range(0,420,8):
-		for col in range(0,500):
+	lastx = [0,0,0]
+	lasty = [0,0,0]
+	for row in range(0,config.canvasHeight,4):
+		for col in range(0,config.canvasWidth):
 			x = col 
 			y = noise.pnoise2(row/config.rowFactor + random.random()*.0, (col+ config.scroll)/config.colFactor + random.random()*.0, octv) * config.amplitude + row
 
 			r = 255
 			g = round(math.sin((col/config.rowFactor)+.1) * 200)
 			b = 50
-			config.draw.rectangle((x, y, x+1, y+1), fill=(r,g,b,150))
+			#config.draw.rectangle((x, y, x+1, y+1), fill=(r,g,b,150))
+			if col != 0 :
+				config.draw.line((lastx[0],lasty[0],x,y), fill = (r,g,b,150))
+			lastx = [x,x,x]
+			lasty = [y,y,y]
 		#octv += 1
 	config.scroll += 2
 
@@ -122,6 +201,7 @@ def main(run=True):
 	config.markSize = int(workConfig.get("noisescroller", "markSize"))
 	config.scroll = 0
 
+	config.function = (workConfig.get("noisescroller", "function"))
 
 	config.bgColorVals = (workConfig.get("noisescroller", "bgColor")).split(",")
 	config.bgColor = tuple(map(lambda x: int(x), config.bgColorVals))

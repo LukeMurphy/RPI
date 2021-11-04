f7e27f91fe251fda6c62bc3a53a2de8fa89f49ea
Luke Murphy <web@lukelab.com>
2021-11-04 10:26:44 -0400

    Getting ready for production of tower-line

--- cntrlscripts/restart_tower_sequencer-2.sh
+++
@@ -1,5 +0,0 @@
-#
-ps -ef | pgrep python | xargs kill -9;
-sleep 1
-python3 /Users/lamshell/Documents/Dev/RPI/sequence-player.py -mname d57 -path /Users/lamshell/Documents/Dev/RPI -cfg sequencer/seq-manifest-tower-line-2.cfg
-#/usr/bin/python3 /home/daemon60/Documents/RPI/sequence-player.py -mname d57 -path /home/daemon60/Documents/RPI -cfg prod/seq-campfire-2.1.0.cfg "$@"

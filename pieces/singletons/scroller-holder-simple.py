ecfbb97f935aebfa91690474b2503f949e542927
Luke Murphy <web@lukelab.com>
2022-02-09 13:38:27 -0500

    TESTING

--- pieces/singletons/scroller-holder-simple.py
+++ pieces/singletons/scroller-holder-simple.py
@@ -164,9 +164,14 @@ def makeBackGround(drawRef, n=1):
 			225,
 		)
 
-		if random.random() < .2 :
+		if random.random() < .5:
 
-			fillClr = (round(random.uniform(0,255) * config.brightness),round(random.uniform(0,255) * config.brightness),round(random.uniform(0,255) * config.brightness),round(random.uniform(0,255))
+			fillClr = (
+				round(random.uniform(0,255) * config.brightness),
+				round(random.uniform(0,150) * config.brightness),
+				round(random.uniform(0,155) * config.brightness),
+				255
+					)
 
 		#drawRef.rectangle((0, 0, 0 + 1, config.canvasHeight), fill = None, outline = (255,0,0,255))
 

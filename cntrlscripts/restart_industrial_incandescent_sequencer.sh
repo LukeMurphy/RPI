9b01f48d79256babee90308d7f3771b85b211940
Luke Murphy <web@lukelab.com>
2022-07-17 13:01:48 -0400

    ==> rming  a file from work ==> preparing a sequnce file for industrial incandescent for testing

---
+++ cntrlscripts/restart_industrial_incandescent_sequencer.sh
@@ -0,0 +1,23 @@
+#
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+echo "*****************************"
+
+ps -ef | pgrep -f player | xargs sudo kill -9;
+ps -ef | pgrep -f player | xargs sudo kill -9;
+sleep 1
+
+python3 /Users/lamshell/Documents/Dev/RPI/sequence-player.py -mname d57 -path /Users/lamshell/Documents/Dev/RPI/ -cfg __in_progress/industrial-incandescent--manifest.cfg
+#/usr/bin/python3 /home/daemon60/Documents/RPI/sequence-player.py -mname d57 -path /home/daemon60/Documents/RPI -cfg prod/seq-campfire-2.1.0.cfg "$@"

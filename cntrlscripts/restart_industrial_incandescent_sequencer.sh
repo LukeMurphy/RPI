a078fd1ca47d6ca85204f3cb890325fdc2d51413
Luke Murphy <web@lukelab.com>
2022-07-17 14:41:33 -0400

    ==> adding Industrial Incandescent production configs

--- cntrlscripts/restart_industrial_incandescent_sequencer.sh
+++ cntrlscripts/restart_industrial_incandescent_sequencer.sh
@@ -19,5 +19,5 @@ ps -ef | pgrep -f player | xargs sudo kill -9;
 ps -ef | pgrep -f player | xargs sudo kill -9;
 sleep 1
 
-python3 /Users/lamshell/Documents/Dev/RPI/sequence-player.py -mname d57 -path /Users/lamshell/Documents/Dev/RPI/ -cfg __in_progress/industrial-incandescent--manifest.cfg
-#/usr/bin/python3 /home/daemon60/Documents/RPI/sequence-player.py -mname d57 -path /home/daemon60/Documents/RPI -cfg prod/seq-campfire-2.1.0.cfg "$@"
+#python3 /Users/lamshell/Documents/Dev/RPI/sequence-player.py -mname d57 -path /Users/lamshell/Documents/Dev/RPI/ -cfg __in_progress/industrial-incandescent--manifest.cfg
+/usr/bin/python3 /home/daemon40/Documents/RPI/sequence-player.py -mname d40 -path /home/daemon40/Documents/RPI -cfg prod/industrial-incandescent--manifest.cfg "$@"

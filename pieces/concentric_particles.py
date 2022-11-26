1c52159e1067d4833b297ce14e5ec6ba6c8ace98
Luke Murphy <web@lukelab.com>
2022-11-26 14:12:20 -0500

    cleanups and refinements

--- pieces/concentric_particles.py
+++ pieces/concentric_particles.py
@@ -6,6 +6,9 @@ import time
 from modules import colorutils
 from PIL import Image, ImageDraw, ImageEnhance, ImageFont, ImageOps
 
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
 class Director:
 	"""docstring for Director"""
 
@@ -28,11 +31,14 @@ class Director:
 		self.checkTime()
 
 
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
 class ParticleDot:
 	def __init__(self):
 		pass
 
-	def setUp(self,p,n):
+	def setUp(self, p, n):
 		# variation in initial velocity
 		direction = 1 if p.directionProb < .5 else -1
 		orbit = True if p.orbitProb > .5 else False
@@ -43,17 +49,17 @@ class ParticleDot:
 		r = int(random.uniform(0, 255) * p.brightness)
 		g = int(random.uniform(0, 200) * p.brightness)
 		b = int(random.uniform(0, 255) * p.brightness)
-		radius = random.uniform(1,p.maxRadius)
-		rSpeed = random.uniform(100,200)/radius/10.0
+		radius = random.uniform(1, p.maxRadius)
+		rSpeed = random.uniform(100, 200)/radius/10.0
 
 		xPos = p.x
 		yPos = p.y
 		angle = p.angle * n
 
-		if direction == -1 :
+		if direction == -1:
 			xPos = round(random.uniform(0, config.canvasWidth))
 			yPos = round(random.uniform(0, config.canvasHeight))
-			angle = math.atan2(yPos - p.y ,xPos - p.x)
+			angle = math.atan2(yPos - p.y, xPos - p.x)
 			vx = math.cos(angle) * fx * direction
 			vy = math.sin(angle) * fy * direction
 
@@ -73,6 +79,9 @@ class ParticleDot:
 		self.sizeNum = 1 if random.random() < .5 else 2
 
 
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
 class ParticleSystem:
 	def __init__(self, config):
 		self.config = config
@@ -84,38 +93,37 @@ class ParticleSystem:
 		self.initXRange = [config.initXRangeMin, config.initXRangeMax]
 		self.initYRange = [config.initYRangeMin, config.initYRangeMax]
 
-
 	def setNewAttributes(self):
-		self.bands = round(random.uniform(12,24))
-		self.wBase = round(random.uniform(220,config.canvasWidth))
+		self.bands = round(random.uniform(12, 24))
+		self.wBase = round(random.uniform(220, config.canvasWidth))
 
 		self.xSpeed = random.random()/2.0
 		self.ySpeed = random.random()
 		self.ySpeed = 0
 
-
 		self.radialsArray = []
-		self.radials = round(random.uniform(120,300))
+		self.radials = round(random.uniform(120, 300))
 		self.rads = 2 * math.pi/self.radials
+
+		self.angleOffset = 0.0
+		self.angleOffsetSpeed = random.uniform(0,math.pi/300)
 		innerRadius = self.wBase/3
 		outerRadius = self.wBase
 		skipRatio = random.random() + .3
-		for i in range(0,self.radials) :
-			ir = innerRadius + random.uniform(-50,50)
-			outr = outerRadius + random.uniform(-50,50)
+		for i in range(0, self.radials):
+			ir = innerRadius + random.uniform(-50, 50)
+			outr = outerRadius + random.uniform(-50, 50)
 			skip = 0 if random.random() < skipRatio else 1
-			self.radialsArray.append([ir,outr,skip])
-	
+			self.radialsArray.append([ir, outr, skip])
 
-	def setCenter(self) :
+	def setCenter(self):
 		# initial center position
 		self.x = round(random.uniform(self.initXRange[0], self.initXRange[1]))
 		self.y = round(random.uniform(self.initYRange[0], self.initYRange[1]))
 
-
 	def setUp(self):
 
-		self.directionProb = random.uniform(.4,.6)
+		self.directionProb = random.uniform(.4, .6)
 		self.orbitProb = random.random()
 		# Number of sparks
 		self.p = int(5 + (random.uniform(config.minParticles, config.maxParticles)))
@@ -158,39 +166,27 @@ class ParticleSystem:
 
 		dx = config.canvasWidth - self.x
 		dy = config.canvasHeight - self.y
-		self.maxRadius = math.sqrt( config.canvasWidth*config.canvasWidth + config.canvasHeight*config.canvasHeight) * 2.5
+		self.maxRadius = math.sqrt(config.canvasWidth*config.canvasWidth +
+								   config.canvasHeight*config.canvasHeight) * 2.5
 
 		for n in range(0, self.p):
 			pDot = ParticleDot()
-			pDot.setUp(self,n)
+			pDot.setUp(self, n)
 			self.particles.append(pDot)
 
-
 	def move(self):
-
 
 		self.x += self.xSpeed
 		self.y += self.ySpeed
 
-		if self.x  > config.canvasWidth - round(self.wBase/4):
+		if self.x > config.canvasWidth - round(self.wBase/4):
 			self.xSpeed = 0
 
-		sumOfDone = 0
-		for q in range(0, self.p):
-			sumOfDone += self.particles[q].done
-
-		if sumOfDone >= self.p - config.numberDone:
-			self.done = True
-
-		if random.random() > 0.996 and self.config.sideWind == True:
-			pass
-			#config.vx = round(2 - random.random() * 4)
-			# useSideWind = True
 
 		for q in range(0, self.p):
 			ref = self.particles[q]
 
-			if ref.mode == 1 :
+			if ref.mode == 1:
 				ref.xPos += ref.vx
 				ref.yPos += ref.vy
 
@@ -199,15 +195,14 @@ class ParticleSystem:
 
 				r = round(math.sqrt(dx*dx + dy*dy))
 
-				if r > ref.radius  and ref.orbit == True :
+				if r > ref.radius and ref.orbit == True:
 					#print(r, ref.radius)
 					ref.mode = 0
 
-			else :
-				ref.xPos = self.x + ref.radius  * math.cos(ref.angle) * .2
-				ref.yPos = self.y + ref.radius  * math.sin(ref.angle) * .2
+			else:
+				ref.xPos = self.x + ref.radius * math.cos(ref.angle) * .2
+				ref.yPos = self.y + ref.radius * math.sin(ref.angle) * .2
 				ref.angle += ref.rSpeed
-
 
 			'''
 			'''
@@ -241,9 +236,6 @@ class ParticleSystem:
 				self.deacellerationx = 0.75
 			'''
 
-			if sumOfClrs < 10 and random.random() > 0.95:
-				self.config.traces = False
-
 			# Sparkles !!
 			if random.random() < config.sparkleProb:
 				r = int(220 * self.sparkleBrightness)
@@ -262,10 +254,12 @@ class ParticleSystem:
 			):
 				try:
 
-					if ref.sizeNum == 2 :
-						config.draw.rectangle( (round(xDisplayPos), round(yDisplayPos), round(xDisplayPos)+1, round(yDisplayPos)+0), fill=(r, g, b,255) )
-					else :
-						config.draw.rectangle( (round(xDisplayPos), round(yDisplayPos), round(xDisplayPos)+0, round(yDisplayPos)+0), fill=(r, g, b,255) )
+					if ref.sizeNum == 2:
+						config.draw.rectangle((round(xDisplayPos), round(yDisplayPos), round(
+							xDisplayPos)+1, round(yDisplayPos)+0), fill=(r, g, b, 255))
+					else:
+						config.draw.rectangle((round(xDisplayPos), round(yDisplayPos), round(
+							xDisplayPos)+0, round(yDisplayPos)+0), fill=(r, g, b, 255))
 					#config.image.putpixel((round(xDisplayPos), round(yDisplayPos)), (r, g, b))
 					#config.image.putpixel((round(xDisplayPos)+1, round(yDisplayPos)), (r, g, b))
 					#config.image.putpixel((round(xDisplayPos), round(yDisplayPos)+1), (r, g, b))
@@ -276,7 +270,7 @@ class ParticleSystem:
 			'''
 			'''
 			if (
-	
+
 				xDisplayPos > config.canvasWidth
 				or yDisplayPos > config.canvasHeight
 				or yDisplayPos < 0
@@ -284,43 +278,14 @@ class ParticleSystem:
 			):
 				ref.setUp(self, ref.id)
 
-
-			if random.random() < config.particleResetProb :
+			if random.random() < config.particleResetProb:
 				ref.setUp(self, ref.id)
 
 
-def drawElement():
-	global config
-	return True
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 
-def redraw():
-	#
-	global config
-
-
-def changeColor():
-	pass
-	return True
-
-
-def changeCall():
-	pass
-	return True
-
-
-def runWork():
-	global redrawSpeed
-	global sprkl
-	redrawSpeed = 0.02
-	while True:
-		config.directorController.checkTime()
-		if config.directorController.advance == True:
-			iterate()
-		time.sleep(redrawSpeed)
-
-
-def drawBands(p) :
+def drawBands(p):
 
 	wBase = p.wBase
 	wDiff = round(wBase / p.bands)
@@ -328,10 +293,19 @@ def drawBands(p) :
 	aBase = 0
 	aDiff = 10
 
-	bBAse = 10
-	bDiff = 5
+	rBase = config.rBase
+	gBase = config.gBase
+	bBase = config.bBase
 
-	for i in range(0,p.bands) :
+	rBase2 = config.rBase2
+	gBase2 = config.gBase2
+	bBase2 = config.bBase2
+
+	rDiff = config.rDiff
+	gDiff = config.gDiff
+	bDiff = config.bDiff
+
+	for i in range(0, p.bands):
 
 		w = wBase - i * wDiff
 		x0 = p.x - w/2
@@ -341,73 +315,82 @@ def drawBands(p) :
 
 		a = round(config.fadeRate + aBase) if config.fadeRate > aBase else config.fadeRate
 
-		config.draw.ellipse((x0,y0,x1,y1), fill =(4,4,bBAse,round(a) ))
+		config.draw.ellipse((x0, y0, x1, y1), fill=(4, 4, bBase, round(a)))
 
 		if i == 1:
-			config.draw.ellipse((x0,y0,x1,y1), fill =(20,14,bBAse,round(a ) ))
+			config.draw.ellipse((x0, y0, x1, y1), fill=(rBase, gBase, bBase, round(a)))
 
 		if i == 0 or i == 12:
-			config.draw.ellipse((x0,y0,x1,y1), fill =(30,24,bBAse,round(a ) ))
-		bBAse += bDiff
+			config.draw.ellipse((x0, y0, x1, y1), fill=(rBase2, gBase2, bBase, round(a)))
+		rBase += rDiff
+		gBase += gDiff
+		bBase += bDiff
 
 	i = 0
-	for n in range(0, len(p.radialsArray)) :
-		x0 = math.cos(i * p.rads) * p.radialsArray[n][0] + p.x
-		y0 = math.sin(i * p.rads) * p.radialsArray[n][0] + p.y
-		x1 = math.cos(i * p.rads) * p.radialsArray[n][1] + p.x
-		y1 = math.sin(i * p.rads) * p.radialsArray[n][1] + p.y	
+	p.angleOffset += p.angleOffsetSpeed
+	for n in range(0, len(p.radialsArray)):
+		a = i * p.rads + p.angleOffset
+		x0 = math.cos(a) * p.radialsArray[n][0] + p.x
+		y0 = math.sin(a) * p.radialsArray[n][0] + p.y
+		x1 = math.cos(a) * p.radialsArray[n][1] + p.x
+		y1 = math.sin(a) * p.radialsArray[n][1] + p.y
 		i += 1
-		if p.radialsArray[n][2] == 0 :
-			config.draw.line((x0,y0,x1,y1), fill =(40,30,0,25))
+		if p.radialsArray[n][2] == 0:
+			config.draw.line((x0, y0, x1, y1), fill=(50, 30, 0, 75))
+
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+def runWork():
+	global redrawSpeed
+	global PS
+	redrawSpeed = 0.02
+	while True:
+		config.directorController.checkTime()
+		if config.directorController.advance == True:
+			iterate()
+		time.sleep(redrawSpeed)
 
 
 def iterate():
 	global config
-	global sprkl
+	global PS
 	config.vx = 0
-
-	if random.random() > 0.995 and config.traces == True:
-		config.sideWind = True
 
 	# Fade out the sparkle
 
 	config.draw.rectangle(
 		(0, 0, config.canvasWidth, config.canvasHeight),
-		fill=(config.bgColor[0],config.bgColor[1],config.bgColor[2], round(config.fadeRate)),
+		fill=(config.bgColor[0], config.bgColor[1], config.bgColor[2], round(config.fadeRate)),
 	)
 
 
-	config.sideWind = False
+	drawBands(PS)
 
-
-	drawBands(sprkl)
-	sprkl.move()
-
+	PS.move()
 
 	config.fadeRate += config.fadeRateDelta
-	if random.random() < config.totalResetProb :
-		print("now")
-		#config.fadeRate = 250
-		#config.fadeRateDelta = 0
-		#config.draw.rectangle((0, 0, config.canvasWidth, config.canvasHeight),fill=(config.bgColor[0],config.bgColor[1],config.bgColor[2], 255))
-		sprkl.setUp()
+	if random.random() < config.totalResetProb:
+		PS.setUp()
 
-
-
-	if config.fadeRate > 255 :
+	if config.fadeRate > 255:
 		config.fadeRate = 30
-		config.fadeRateDelta = random.uniform(.1,2)
+		config.fadeRateDelta = random.uniform(.1, 2)
 
-		print (config.fadeRateDelta)
-		if config.fadeRateDelta  <= 1.0 :
-			print("ALL NEW")
-			sprkl.setCenter()
-			sprkl.setNewAttributes()
-			sprkl.setUp()
+		if config.fadeRateDelta <= config.fadeRateNewSystemThreshold:
+			# when the system reaches a visible chaotic crescendo that will last a few seconds, remake the system
+			# behind the chaos and start again - choose a new background, reset the center, set new radial attributes
+			# renew the particle dots that travel
+			#bgChoice = math.floor(random.uniform(0,len(config.bgColorSets)))
+			config.bgColor = random.choice(config.bgColorSets)
+			print(f"ALL NEW {config.bgColor}  {config.fadeRateDelta}")
 
+			PS.setCenter()
+			PS.setNewAttributes()
+			PS.setUp()
 
 	config.render(config.image, 0, 0, config.canvasWidth, config.canvasHeight)
-
 
 	# Done
 
@@ -415,14 +398,11 @@ def iterate():
 def main(run=True):
 	global config
 	global redrawSpeed
-	global sprkl
+	global PS
 
-	config.traces = (workConfig.getboolean("particles", "traces"))
-	config.sideWind = (workConfig.getboolean("particles", "sideWind"))
 
 	config.minParticles = int(workConfig.get("particles", "minParticles"))
 	config.maxParticles = int(workConfig.get("particles", "maxParticles"))
-	config.numberDone = 1
 
 	config.speedFactorMin = float(workConfig.get("particles", "speedFactorMin"))
 	config.speedFactorMax = float(workConfig.get("particles", "speedFactorMax"))
@@ -432,27 +412,48 @@ def main(run=True):
 	config.initYRangeMin = int(workConfig.get("particles", "initYRangeMin"))
 	config.initYRangeMax = int(workConfig.get("particles", "initYRangeMax"))
 
-	config.action = "other"
 	config.systemRotation = float(workConfig.get("particles", "systemRotation"))
 
-	config.bgColor = [1, 1, 2]
+	bgColorSets = (workConfig.get("particles", "bgColorSets")).split(",")
+	config.bgColorSets = []
+	for bg in bgColorSets :
+		bgColor = (workConfig.get(bg, "bgColor")).split(",") 
+		bgColors = list( int(x) for x in   bgColor )
+		config.bgColorSets.append(bgColors)
+
+	# choose the first bg color - generally the dark one
+	config.bgColor = config.bgColorSets[0]
+
+	config.rBase = int(workConfig.get("particles", "rBase"))
+	config.gBase = int(workConfig.get("particles", "gBase"))
+	config.bBase = int(workConfig.get("particles", "bBase"))
+	config.rBase2 = int(workConfig.get("particles", "rBase2"))
+	config.gBase2 = int(workConfig.get("particles", "gBase2"))
+	config.bBase2 = int(workConfig.get("particles", "bBase2"))
+	config.rDiff = int(workConfig.get("particles", "rDiff"))
+	config.gDiff = int(workConfig.get("particles", "gDiff"))
+	config.bDiff = int(workConfig.get("particles", "bDiff"))
+
+
+
 	config.fadeRate = float(workConfig.get("particles", "fadeRate"))
 	config.fadeRateDelta = float(workConfig.get("particles", "fadeRateDelta"))
 	config.sparkleProb = float(workConfig.get("particles", "sparkleProb"))
+	config.fadeRateNewSystemThreshold = float(workConfig.get("particles", "fadeRateNewSystemThreshold"))
 
-	config.particleResetProb =  float(workConfig.get("particles", "particleResetProb"))
-	config.totalResetProb =  float(workConfig.get("particles", "totalResetProb"))
+	config.particleResetProb = float(workConfig.get("particles", "particleResetProb"))
+	config.totalResetProb = float(workConfig.get("particles", "totalResetProb"))
 
 	config.image = Image.new("RGBA", (config.canvasWidth, config.canvasHeight))
 	config.draw = ImageDraw.Draw(config.image)
-	sprkl = ParticleSystem(config)
-	sprkl.setCenter()
-	sprkl.setNewAttributes()
-	sprkl.setUp()
+	PS = ParticleSystem(config)
+	PS.setCenter()
+	PS.setNewAttributes()
+	PS.setUp()
 
 	# managing speed of animation and framerate
 	config.directorController = Director(config)
-	config.directorController.slotRate  = .03
+	config.directorController.slotRate = .03
 
 	if run:
 		runWork()

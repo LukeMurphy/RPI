3acb5041b2caa73f051fc8ed6221dc66f234c1ee
Luke Murphy <web@lukelab.com>
2023-12-22 23:28:18 -0500

    fixing full lister manager to be better

--- cntrlscripts/full_list.py
+++ cntrlscripts/full_list.py
@@ -1,22 +1,3 @@
-"""Summary
-
-Attributes:
-    actionDict1 (TYPE): Description
-    actionDict2 (TYPE): Description
-    commadStringMultiPyth (str): Description
-    commadStringProc (str): Description
-    commadStringPyth (str): Description
-    JavaAppRunning (str): Description
-    Lb1 (TYPE): Description
-    leftBtnPlace (int): Description
-    quitbutton (TYPE): Description
-    root (TYPE): Description
-    scrollbar (TYPE): Description
-    slogan (TYPE): Description
-    sortbutton (TYPE): Description
-    sortDefault (int): Description
-    topBtnPlace (int): Description
-"""
 import os
 from os import listdir
 from os.path import isfile, join, isdir
@@ -31,6 +12,25 @@ from tkmacosx import Button
 
 # from tk import Button
 
+"""Summary
+
+Attributes:
+    actionDict1 (TYPE): Description
+    actionDict2 (TYPE): Description
+    commadStringMultiPyth (str): Description
+    commadStringProc (str): Description
+    commadStringPyth (str): Description
+    JavaAppRunning (str): Description
+    ListBoxOfConfigs (TYPE): Description
+    leftBtnPlace (int): Description
+    quitbutton (TYPE): Description
+    root (TYPE): Description
+    scrollbar (TYPE): Description
+    slogan (TYPE): Description
+    sortbutton (TYPE): Description
+    sortDefault (int): Description
+    topBtnPlace (int): Description
+"""
 
 commadStringProc = ""
 JavaAppRunning = ""
@@ -48,17 +48,12 @@ actionDict2 = [
 
 
 def verify():
-    """Summary
-
-    Returns:
-        TYPE: Description
-    """
     # print("==>",Lb.curselection())
     global actionDict1
     process = False
     configSelected = None
-    if len(list(Lb1.curselection())) > 0:
-        selection = Lb1.curselection()[0]
+    if len(list(ListBoxOfConfigs.curselection())) > 0:
+        selection = ListBoxOfConfigs.curselection()[0]
         configSelected = actionDict1[selection]
         process = True
     # elif len(list(Lb2.curselection())) > 0:
@@ -104,7 +99,6 @@ def execute(configToRun):
 
 
 def action():
-    """Summary"""
     a = verify()
     if a[0] == True:
         # os.system('ps -ef | pgrep -f player | xargs sudo kill -9;')
@@ -114,7 +108,6 @@ def action():
 
 
 def action2():
-    """Summary"""
     global JavaAppRunning
     a = verify()
     if a[0] == True:
@@ -131,17 +124,18 @@ def action2():
 
 
 def stopAll():
-    """Summary"""
     # print("Tkinter is easy to use!")
     os.system("ps -ef | pgrep -f player | xargs sudo kill -9;")
 
 
 def sortByDate():
-    getAllConfigFiles(True)
+    filterText = (T.get("1.0", "end-1c"))
+    getAllConfigFiles(True, False, filterText)
 
 
 def sortByFolder():
-    getAllConfigFiles(False)
+    filterText = (T.get("1.0", "end-1c"))
+    getAllConfigFiles(False, False, filterText)
 
 
 def sortByFolderAndDate():
@@ -164,70 +158,35 @@ def returnFirstElement(l):
 
 
 def returnSecondElement(l):
-    """Summary
-
-    Args:
-        l (TYPE): Description
-
-    Returns:
-        TYPE: Description
-    """
     return l[1]
 
 
-def getAllConfigFiles(dateSort=False, subsortDate=False):
-    """Summary
+def getAllConfigFiles(dateSort=False, subsortDate=False, filterText = ""):
 
-    Args:
-        dateSort (bool, optional): Description
-    """
-
-    global actionDict1, Lb1
+    global actionDict1, ListBoxOfConfigs
     global configPath
     # arr = os.listdir(configPath)
     # Sort the directories by name
     # arr.sort(reverse=False)
     fullList = []
     actionDict1 = []
-
+    
+    filterResults = False
+    if len(filterText) > 1 :
+        filterResults = True
+        
     for root, dirs, files in os.walk(configPath, topdown=False):
         for name in files:
             fullPath = os.path.join(root, name)
             if name.find(".cfg") > 0 and name.find(".py") == -1 and name.find(".DS_Store") == -1:
                 res = os.stat(fullPath)
-                fullList.append((os.path.join(root, name), res.st_mtime, name))
+                
+                if filterResults == False :
+                    fullList.append((os.path.join(root, name), res.st_mtime, name))
+                if filterResults == True :
+                    if name.find(filterText) > 0 or fullPath.find(filterText) > 0:
+                        fullList.append((os.path.join(root, name), res.st_mtime, name))
 
-    # for directory in arr:
-    #     if (
-    #         directory.find(".py") == -1
-    #         and directory.find(".DS_Store") == -1
-    #         and directory.find("_py") == -1
-    #         and directory.find("LED") == -1
-    #     ):
-    #         subArr = os.listdir(configPath + directory)
-    #         subDirectoryList = []
-
-    #         if dateSort == False:
-    #             subArr.sort(reverse=True)
-
-    #         for file in subArr:
-    #             if file.find(".DS_Store") == -1:
-    #                 shortPath = directory + "/" + file
-    #                 if os.path.isfile(configPath + shortPath):
-    #                     res = os.stat(configPath + shortPath)
-    #                     if dateSort == False:
-    #                         subDirectoryList.append((shortPath, res.st_mtime))
-    #                     else:
-    #                         fullList.append((shortPath, res.st_mtime))
-
-    #         if dateSort == False:
-    #             # sorts by date within folder
-    #             if subsortDate == True:
-    #                 subDirectoryList.sort(key=returnSecondElement, reverse=True)
-    #             else:
-    #                 subDirectoryList.sort(reverse=False)
-    #             fullList.extend(subDirectoryList)
-    #             fullList.append({})
 
     # Sort the configs by date descending
     if dateSort == True:
@@ -240,26 +199,31 @@ def getAllConfigFiles(dateSort=False, subsortDate=False):
     for f in fullList:
         fName = f[0].split(configPath)[1].split('/')[0]
         if len(f) > 0:
-            tsTxt = datetime.datetime.fromtimestamp(
-                f[1]).strftime("[%Y-%m-%d %H:%M]")
+            tsTxt = datetime.datetime.fromtimestamp(f[1]).strftime("%Y-%m-%d [%H:%M]")
+            tsTxtVals = tsTxt.split(" ")
             
             # and dateSort != True
-            if fName != lastDir :
+            currentDir = f[0].split(configPath)[1].split('/')[0]
+            currDirLevel2 = f[0].split(configPath)[1].split('/')[1]
+            if currDirLevel2.find(".cfg") <= 0 :
+                    currentDir = currentDir + "/" + currDirLevel2
+            if currentDir != lastDir :
                 actionDict1.append({"": ""})
+                lastDir = currentDir
 
-            lastDir = f[0].split(configPath)[1].split('/')[0]
-            # actionDict1.append({tsTxt + "\t\t" + f[0].split(configPath)[1] + " " + f[2]: f[0]})
-            actionDict1.append({tsTxt + "\t" + f[0].split(f[2])[0].split(configPath)[1]  + " \t\t\t\t \t\t\t\t" + f[2]: f[0]})
-            # actionDict1.append({ "" + tsTxt  + "  " + f[0]  : f[0]})
-
+            actionDict1.append({tsTxt + "\t\t" + f[0].split(f[2])[0].split(configPath)[1]  + " \t\t\t\t \t\t\t\t" + f[2]: f[0]})
         else:
             actionDict1.append({"": ""})
 
-    Lb1.delete(0, END)
+    ListBoxOfConfigs.delete(0, END)
     for i, item in enumerate(actionDict1):
-        Lb1.insert(END, " " + list(item.keys())[0])
+        # print(list(item.keys())[0])
+        ListBoxOfConfigs.insert(END, " " + list(item.keys())[0])
+        ListBoxOfConfigs.itemconfig(END, bg = "yellow" if list(item.keys())[0].find("prod/") > 0 else "white" )
 
 
+# -------------------------------- #
+# Setup the TKINTER window
 root = tk.Tk()
 # frame = tk.Frame(root, bg="darkgray")
 # frame.pack(padx=1, pady=1)
@@ -277,32 +241,33 @@ root.geometry(
         round(1 * screen_height / 2),
     )
 )
+def_font = tk.font.nametofont("TkDefaultFont")
+def_font.config(size=12)
+root.config(bg="white")
 
-Lb1 = Listbox(root, width=90, height=42)
-
+# -------------------------------- #
+# Setup the main listBox widget
+ListBoxOfConfigs = Listbox(root, width=90, height=42, bg="white", foreground="black", bd = False)
 
 for i, item in enumerate(actionDict1):
-    Lb1.insert(END, " " + list(item.keys())[0])
-    Lb1.itemconfig(END, {"bg" : 'red'} )
-    # if len(list(item.keys())[0]) < 0 else "green"
+    ListBoxOfConfigs.insert(END, " " + list(item.keys())[0])
+    ListBoxOfConfigs.itemconfig(END, {"bg" : 'red'} )
 
+ListBoxOfConfigs.place(bordermode=OUTSIDE, x=2, y=10)
 
-# Lb1.pack(side=tk.LEFT, padx=0, ipadx=10)
-# Lb2.pack(side=tk.LEFT, ipadx=10, expand=0)
-Lb1.place(bordermode=OUTSIDE, x=2, y=2)
-
-
+# -------------------------------- #
 scrollbar = Scrollbar(root)
 scrollbar.pack(side=RIGHT, fill=BOTH)
-Lb1.config(yscrollcommand=scrollbar.set)
-scrollbar.config(command=Lb1.yview)
+ListBoxOfConfigs.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=ListBoxOfConfigs.yview)
 
 topBtnPlace = 8
-leftBtnPlace = 580
+leftBtnPlace = 630
 
 # sort by directory is 1 sort all by date is 0
 sortDefault = 1
 
+# -------------------------------- #
 slogan = Button(
     root,
     text="Stop & Run",
@@ -314,16 +279,19 @@ slogan = Button(
 )
 slogan.place(bordermode=OUTSIDE, x=leftBtnPlace, y=topBtnPlace)
 
+# -------------------------------- #
 slogan = Button(
     root, text="Run", width=120, bg="blue", fg="white", borderless=1, command=action
 )
 slogan.place(bordermode=OUTSIDE, x=leftBtnPlace, y=topBtnPlace + 25)
 
+# -------------------------------- #
 openbutton = Button(
     root, text="Open", width=120, bg="blue", fg="white", borderless=1, command=openFile
 )
 openbutton.place(bordermode=OUTSIDE, x=leftBtnPlace, y=topBtnPlace + 50)
 
+# -------------------------------- #
 sortbutton1 = Button(
     root,
     text="Sort By Date",
@@ -335,6 +303,7 @@ sortbutton1 = Button(
 )
 sortbutton1.place(bordermode=OUTSIDE, x=leftBtnPlace, y=topBtnPlace + 100)
 
+# -------------------------------- #
 sortbutton2 = Button(
     root,
     text="Sort by Folder",
@@ -356,7 +325,7 @@ sortbutton2.place(bordermode=OUTSIDE, x=leftBtnPlace, y=topBtnPlace + 125)
 #     command=sortByFolderAndDate,
 # )
 # sortbutton3.place(bordermode=OUTSIDE, x=leftBtnPlace, y=topBtnPlace + 150)
-
+# -------------------------------- #
 slogan = Button(
     root,
     text="Stop All",
@@ -368,12 +337,20 @@ slogan = Button(
 )
 slogan.place(bordermode=OUTSIDE, x=leftBtnPlace, y=topBtnPlace + 175)
 
+# -------------------------------- #
 quitbutton = Button(
     root, text="QUIT", width=120, bg="blue", fg="white", borderless=1, command=quit
 )
 quitbutton.place(bordermode=OUTSIDE, x=leftBtnPlace, y=topBtnPlace + 200)
 
+# -------------------------------- #
+# Filter text box
+T = Text(root, height = 1, width = 82, bg="white", fg="black", bd=False)
+T.place(bordermode=OUTSIDE, x=2, y=2)
+
+# -------------------------------- #
 # sort by date = True,  
 getAllConfigFiles(True, True)
 
+# -------------------------------- #
 root.mainloop()

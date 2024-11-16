2681af5a1f50c3751d70decc0fdadfef6b0f09a3
Luke Murphy <web@lukelab.com>
2024-11-15 23:51:16 -0500

    minor changes

--- modules/configuration.py
+++ modules/configuration.py
@@ -28,7 +28,7 @@ class bcolors:
     OKBLUE = "\033[94m"
     OKGREEN = "\033[92m"
     WARNING = "\033[93m"
-    FAIL = "\033[91m"
+    FAIL = "\033[95m"
     ENDC = "\033[0m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"

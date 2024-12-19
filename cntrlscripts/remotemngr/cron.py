4e3f01238931063562c89288fdbb6e44e08ea7b5
Luke Murphy <web@lukelab.com>
2024-12-18 23:29:08 -0500

    setting up for daemon102 remote

--- cntrlscripts/remotemngr/cron.py
+++ cntrlscripts/remotemngr/cron.py
@@ -11,8 +11,8 @@ import time
 # This is probably the file to set as the machine's startup desktop 
 # if the machine is under remote control
 
-# initPath = '/home/daemon90/Documents/remotemngr/remotecontrol-remote-to-lukelab.sh'
-initPath = '/Users/lamshell/Documents/Dev/LEDELI/remotemngr/remotecontrol-remote-to-lukelab.sh'
+initPath = '/home/daemon102/Documents/remotemngr/remotecontrol-remote-to-lukelab.sh'
+# initPath = '/Users/lamshell/Documents/Dev/LEDELI/remotemngr/remotecontrol-remote-to-lukelab.sh'
 timeToCheck = 15
 
 def runScript(arg='startup') :

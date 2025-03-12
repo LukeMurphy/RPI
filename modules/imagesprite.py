5a42c46754775cbba0d0ad528800d09d560f12a8
Luke Murphy <web@lukelab.com>
2025-03-11 22:06:00 -0400

    fixes for some legacy items and new production updates

--- modules/imagesprite.py
+++ modules/imagesprite.py
@@ -418,7 +418,7 @@ class ImageSprite:
                 # print(tartClr)
                 region = region.point(lambda i: tartClr if (i >= 0 and i < 10) else i)
 
-
+            region = region.convert("RGBA")
             self.image.paste(region, box, region)
 
     """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" ""

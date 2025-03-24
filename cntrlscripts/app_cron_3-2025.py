981e7c3b8d94d3dd8302b27a7c7cade19cd4772c
Luke Murphy <web@lukelab.com>
2025-03-24 14:58:07 -0400

    control script tests

--- cntrlscripts/app_cron_3-2025.py
+++ cntrlscripts/app_cron_3-2025.py
@@ -11,30 +11,17 @@ import time
 # This is probably the file to set as the machine's startup desktop
 # if the machine is under remote control
 path = "/home/daemon104/Documents/RPI/cntrlscripts/"
-app1 = f"{path}start_app1.sh"
-app1_endcmd = f"{path}shut_player_down.sh"
-
-app2 = f"{path}start_app2.sh"
-app2_endcmd = f"{path}shut_electronplayer_down.sh"
-
-appList = [[app1, app1_endcmd],[app2,app2_endcmd]]
-appListIndex = 1
-appListPrevIndex = 0
+appChanger = f"{path}app_changer.sh"
+appListIndex = 0
 
 timeToCheck = 15
 
 def runScript(arg="startup"):
-    global timeToCheck,appList,appListIndex,appListPrevIndex
+    global timeToCheck,appListIndex
     try:
-        execCmd1 = f"{appList[appListIndex][0]}"
-        print(execCmd1)
-        os.system(execCmd1)
-
-        execCmd2 = f"{appList[appListPrevIndex][1]}"
-        print(execCmd2)
-        os.system(execCmd2)
-
-        appListPrevIndex = appListIndex
+        execCmd = f"{appList[appListIndex][0]} -{appListIndex}"
+        print(execCmd)
+        os.system(execCmd)
         appListIndex = 1 if appListIndex == 0 else 0
     except Exception as e:
         print("There was an issue:")
@@ -42,6 +29,5 @@ def runScript(arg="startup"):
     # end try
     time.sleep(timeToCheck)
     runScript("cron")
-
 
 runScript("startup")

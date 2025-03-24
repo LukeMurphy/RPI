212e93984856ace3d2f33ec23612618528e082b3
Luke Murphy <web@lukelab.com>
2025-03-24 15:17:13 -0400

    sigh....

--- cntrlscripts/app_changer.sh
+++ cntrlscripts/app_changer.sh
@@ -1,11 +1,12 @@
 if [ $1 = "1" ]; then
     echo "==>starting "
-    exec /home/daemon104/Documents/electron-quick-start-linux-x64/electron-quick-start
+    exec /home/daemon104/Documents/electron-quick-start-linux-x64/electron-quick-start&
+    sleep .5
     ps -ef | pgrep -f player.py | xargs kill -9;
 fi
-
 if [ $1 = "2" ]; then
     echo "==>starting "
-    /usr/bin/python3 /home/daemon104/Documents/RPI/player.py -cfg prod/p4-6x8-paintings.cfg
+    /usr/bin/python3 /home/daemon104/Documents/RPI/player.py -cfg prod/p4-6x8-paintings.cfg&
+    sleep .5
     ps -ef | pgrep -f electron | xargs kill -9;
 fi

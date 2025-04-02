33aeb22fdddf83acfac4f5efcd97a89b953a9680
Luke Murphy <web@lukelab.com>
2025-04-02 10:38:08 -0400

    name change

--- cntrlscripts/app_changer.sh
+++ cntrlscripts/app_changer.sh
@@ -6,13 +6,13 @@ if [ $1 = "1" ]; then
 fi
 if [ $1 = "2" ]; then
     echo "==>starting "
-    /usr/bin/python3 /home/daemon104/Documents/RPI/player.py -cfg prod/p4-6x8-paintings.cfg&
+    /usr/bin/python3 /home/daemon104/Documents/RPI/player.py -cfg prod/p4-6x8-actionmarks.cfg&
     sleep .5
     ps -ef | pgrep -f electron | xargs kill -9;
 fi
 if [ $1 = "3" ]; then
     echo "==>starting "
-    /usr/bin/python3 /home/daemon104/Documents/RPI/player.py -cfg prod/p4-6x8-paintings-scribsca-1_1.cfg&
+    /usr/bin/python3 /home/daemon104/Documents/RPI/player.py -cfg prod/p4-6x8-paintings-actionmarks-1_1.cfg&
     sleep .5
     ps -ef | pgrep -f electron | xargs kill -9;
 fi

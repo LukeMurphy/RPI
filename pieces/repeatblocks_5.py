c60b519de1635ac41e387245d00f9caf20ad0686
Luke Murphy <web@lukelab.com>
2025-07-29 22:09:01 -0400

    production adjustments

--- pieces/repeatblocks_5.py
+++ pieces/repeatblocks_5.py
@@ -1517,8 +1517,9 @@ def main(run=True):
     #         config.render(config.renderImageFull, 0, 0)
     # """
 
-    # prints out everything in the config global
-    config.debugSelf()
+    if config.setupDeBug:
+        # prints out everything in the config global
+        config.debugSelf()
 
     if run:
         runWork()

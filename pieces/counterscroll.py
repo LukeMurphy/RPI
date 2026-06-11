6cf98c045f74d35865a887239aad1f4cf7138a8e
Luke Murphy <web@lukelab.com>
2026-06-11 15:51:34 -0400

    fonts

--- pieces/counterscroll.py
+++ pieces/counterscroll.py
@@ -52,7 +52,7 @@ class ScrollMessage:
         # draw the message to get its size
         if config.sansSerif:
             # font = ImageFont.truetype(config.path + "/assets/fonts/freefont/FreeSansBold.ttf", config.fontSize)
-            font = ImageFont.truetype(config.path + "/assets/fonts/freefont/RobotoCondensed-Bold.ttf", config.fontSize)
+            font = ImageFont.truetype(config.path + "/assets/fonts/roboto/RobotoCondensed-Bold.ttf", config.fontSize)
         else:
             font = ImageFont.truetype(
                 config.path + "/assets/fonts/freefont/FreeSerifBold.ttf",
